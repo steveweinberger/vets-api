fb4f874df3210664166e79391d3b8cdce80f7a84
Lihan Li <frankieteardrop@gmail.com>
2019-12-05 07:55:49 -0800

    f

--- spec/lib/vet360/address_validation/service_spec.rb
+++ spec/lib/vet360/address_validation/service_spec.rb
@@ -114,8 +114,8 @@ describe Vet360::AddressValidation::Service do
     context 'with an invalid address' do
       it 'returns an error' do
         VCR.use_cassette(
-          'vet360/address_validation/candidate_no_match',
-          VCR::MATCH_EVERYTHING
+          'vet360/address_validation/candidate_no_match2',
+          record: :once
         ) do
           expect { described_class.new.candidate(invalid_address) }.to raise_error(
             Common::Exceptions::BackendServiceException
