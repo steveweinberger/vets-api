3771a40c126491fa7789e6abfb676b44d74257df
Lihan Li <frankieteardrop@gmail.com>
2019-12-06 11:10:50 -0800

    update candidate no match

--- spec/lib/vet360/address_validation/service_spec.rb
+++ spec/lib/vet360/address_validation/service_spec.rb
@@ -111,12 +111,21 @@ describe Vet360::AddressValidation::Service do
 
   describe '#candidate' do
     context 'with an invalid address' do
-      it 'returns an empty hash' do
+      it 'returns error messages' do
         VCR.use_cassette(
-          'vet360/address_validation/candidate_no_match2',
-          record: :once
+          'vet360/address_validation/candidate_no_match',
+          VCR::MATCH_EVERYTHING
         ) do
-          expect(described_class.new.candidate(invalid_address)).to eq({})
+          expect(described_class.new.candidate(invalid_address)).to eq(
+            {"candidate_addresses"=>
+            [{"address"=>
+               {"state_province"=>{"name"=>"Maryland", "code"=>"MD"}, "country"=>{"name"=>"United States", "code"=>"USA", "fips_code"=>"US", "iso2_code"=>"US", "iso3_code"=>"USA"}, "address_line1"=>"Sdfdsfsdf", "city"=>"Sparks Glencoe", "zip_code5"=>"21152"},
+              "geocode"=>{"calc_date"=>"2019-12-06T18:47:31+00:00", "location_precision"=>0.0, "latitude"=>39.5412, "longitude"=>-76.6676},
+              "address_meta_data"=>{"confidence_score"=>0.0, "address_type"=>"Unknown", "delivery_point_validation"=>"MISSING_ZIP", "validation_key"=>1499210293},
+              "messages"=>
+               [{"code"=>"ADDRVAL112", "key"=>"AddressCouldNotBeFound", "severity"=>"WARN", "text"=>"The Address could not be found"},
+                {"code"=>"ADDR306", "key"=>"lowConfidenceScore", "severity"=>"WARN", "text"=>"Vet360 Validation Failed: Confidence Score less than 80"}]}]}
+          )
         end
       end
     end
