b14b4ff00ae63c241bc36ea286d7e8cc01170147
Philip Becker <philip.becker@oddball.io>
2020-02-01 11:08:55 -0500

    basic framework for a search api

---
+++ app/controllers/external_api/external_api_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+module ExtrnalApi::ApplicationController < ActionController::API
+  # shared concerns (such as security)
+end
---
+++ app/controllers/external_api/search/search_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ExtrnalApi
+  class SearchController < ApplicationController
+
+  end
+end
