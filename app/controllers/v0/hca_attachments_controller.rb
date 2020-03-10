ff97b1a1f0f0b1752ecf79728d1da980cf6a76da
Philip Becker <philip.becker@oddball.io>
2020-03-10 10:27:09 -0400

    use VetsApiController on hca_attachments

--- app/controllers/v0/hca_attachments_controller.rb
+++ app/controllers/v0/hca_attachments_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module V0
-  class HcaAttachmentsController < ApplicationController
+  class HcaAttachmentsController < VetsApiController
     include FormAttachmentCreate
 
     FORM_ATTACHMENT_MODEL = HcaAttachment
