51465ef650899c14f9a3f68ce75f379b050201b1
Philip Becker <philip.becker@oddball.io>
2020-03-13 14:00:19 -0400

    log an the lack of CSRF instead of raising an error

--- app/controllers/vets_api_controller.rb
+++ app/controllers/vets_api_controller.rb
@@ -16,8 +16,9 @@ class VetsApiController < ApplicationController
 
   def validate_csrf_token!
     if request.headers['X-CSRF-Token'].nil? || request.headers['X-CSRF-Token'] != cookies['X-CSRF-Token']
-      # TODO log there was a request without the token, then when this is going to be enforced return a meaningful error
-      raise ActionController::InvalidAuthenticityToken
+      # for now we are just logging when there's no CSRF protection
+      log_message_to_sentry('Request susceptible to CSRF', :info, { controller: self.class, action: action_name })
+      # when this is going to be enforced return a meaningful error (and turn up logging level)
     end
   end
 end
