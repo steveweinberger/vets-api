bc09e502ba5cfef17a9ffc2ac7c659d4a44868dd
Steven Garcia <sgarcia@thoughtworks.com>
2020-10-21 12:00:54 -0700

    [department-of-veterans-affairs/orchid#121] Created initial inquiry attachment upload controller (WIP); need to explore how to map routes and how to sync back to UI

---
+++ app/controllers/v0/ask/inquiry_documents_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module V0
+  class InquiryDocumentsController < ApplicationController
+    skip_before_action(:authenticate)
+
+    def create
+      attachment = klass.new(form_id: form_id)
+      # add the file after so that we have a form_id and guid for the uploader to use
+      attachment.file = params['file']
+      raise Common::Exceptions::ValidationErrors, attachment unless attachment.valid?
+
+      attachment.save
+      render json: attachment
+    end
+
+    private
+
+    def klass
+      case form_id
+      when '0873'
+        PersistentAttachments::InquiryAttachments
+      end
+    end
+
+    def form_id
+      params[:form_id].upcase
+    end
+  end
+end
