8d71a125b05de5d9dcd643b8bb29a206365d1815
Lihan Li <frankieteardrop@gmail.com>
2020-12-31 11:31:25 -0800

    f

--- spec/lib/bgs/service_spec.rb
+++ spec/lib/bgs/service_spec.rb
@@ -30,7 +30,10 @@ RSpec.describe BGS::Service do
       VCR.config do |c|
         c.allow_http_connections_when_no_cassette = true
       end
-      res = bgs_service.send(:service).ddeft.find_bank_name_by_routng_trnsit_nbr('122400724')
+      VCR.use_cassette('bgs/service/find_bank_name_by_routng_trnsit_nbr', record: :once) do
+        res = bgs_service.send(:service).ddeft.find_bank_name_by_routng_trnsit_nbr('122400724')
+      end
+
       binding.pry; fail
     end
 
