4bae0c133f16740622cd34dc3fefeb8746f2e64e
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-13 15:41:00 -0500

    write ViewportCollection#parse_report

--- app/workers/cypress_viewport_environment_variables/viewport_collection.rb
+++ app/workers/cypress_viewport_environment_variables/viewport_collection.rb
@@ -13,5 +13,32 @@ module CypressViewportEnvironmentVariables
       @google_analytics_report = report
       parse_report
     end
+
+    private
+
+    def parse_report
+      google_analytics_report.data.rows.each do |row|
+        device = row.dimensions.first
+
+        case device
+        when 'mobile'
+          if top_mobile_viewports.size < 5
+            top_mobile_viewports << create_viewport_hash(row)
+          end
+        when 'tablet'
+          if top_tablet_viewports.size < 5
+            top_tablet_viewports << create_viewport_hash(row)
+          end
+        when 'desktop'
+          if top_desktop_viewports.size < 5
+            top_desktop_viewports << create_viewport_hash(row)
+          end
+        end
+
+        break if [top_mobile_viewports,
+                  top_tablet_viewports,
+                  top_desktop_viewports].all? { |array| array.size >= 5 }
+      end
+    end
   end
 end
