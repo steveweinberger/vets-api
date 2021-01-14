11112c01960a6ce10e41ebe74f7343744b2ae00f
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-14 11:43:07 -0500

    refactor ViewportCollection

--- app/workers/cypress_viewport_environment_variables/viewport_collection.rb
+++ app/workers/cypress_viewport_environment_variables/viewport_collection.rb
@@ -5,20 +5,15 @@ module CypressViewportEnvironmentVariables
 
     NUMBER_OF_TOP_VIEWPORTS = 5
 
-    attr_reader :start_date, :end_date, :total_users,
-                :google_analytics_viewport_report, :top_mobile_viewports,
-                :top_tablet_viewports, :top_desktop_viewports
+    attr_reader :start_date, :end_date, :total_users, :viewports
     
     def initialize(start_date:, end_date:, user_report:, viewport_report:)
-      @top_mobile_viewports = []
-      @top_tablet_viewports = []
-      @top_desktop_viewports = []
       @start_date = start_date
       @end_date = end_date
       @total_users = parse_user_report_for_total_users(user_report)
-      @google_analytics_viewport_report = viewport_report
-      parse_google_analytics_viewport_report_to_create_top_viewport_collections
-      update_viewports_with_rank_in_each_top_viewport_collection
+      @viewports = { mobile: [], tablet: [], desktop: [] }
+      parse_viewport_report_to_populate_viewports(viewport_report)
+      update_viewport_attributes_that_reference_rank
     end
 
     private
@@ -27,30 +22,33 @@ module CypressViewportEnvironmentVariables
       user_report.data.totals.first.values.first.to_f
     end
 
-    def parse_google_analytics_viewport_report_to_create_top_viewport_collections
-      google_analytics_viewport_report.data.rows.each do |row|
+    def parse_viewport_report_to_populate_viewports(viewport_report)
+      viewport_report.data.rows.each do |row|
         device = row.dimensions.first
 
         case device
         when 'mobile'
-          if top_mobile_viewports.size < NUMBER_OF_TOP_VIEWPORTS
-            top_mobile_viewports << make_new_viewport(row)
+          mobile_viewports = viewports[:mobile]
+          if mobile_viewports.size < NUMBER_OF_TOP_VIEWPORTS
+            mobile_viewports << make_viewport(row)
           end
         when 'tablet'
-          if top_tablet_viewports.size < NUMBER_OF_TOP_VIEWPORTS
-            top_tablet_viewports << make_new_viewport(row)
+          tablet_viewports = viewports[:tablet]
+          if tablet_viewports.size < NUMBER_OF_TOP_VIEWPORTS
+            tablet_viewports << make_viewport(row)
           end
         when 'desktop'
-          if top_desktop_viewports.size < NUMBER_OF_TOP_VIEWPORTS
-            top_desktop_viewports << make_new_viewport(row)
+          desktop_viewports = viewports[:desktop]
+          if desktop_viewports.size < NUMBER_OF_TOP_VIEWPORTS
+            desktop_viewports << make_viewport(row)
           end
         end
 
-        break if all_top_viewport_collections_full?
+        break if viewports_full?
       end
     end
 
-    def make_new_viewport(row)
+    def make_viewport(row)
       CypressViewportEnvironmentVariables::
         Viewport.new(start_date: start_date,
                      end_date: end_date,
@@ -58,23 +56,23 @@ module CypressViewportEnvironmentVariables
                      total_users: total_users)
     end
 
-    def all_top_viewport_collections_full?
-      viewport_collections.all? do |viewports|
+    def viewports_full?
+      viewport_types.all? do |viewports|
         viewports.count >= NUMBER_OF_TOP_VIEWPORTS
       end
     end
 
-    def update_viewports_with_rank_in_each_top_viewport_collection
-      viewport_collections.each do |collection|
-        collection.each_with_index do |viewport, index|
+    def update_viewport_attributes_that_reference_rank
+      viewport_types.each do |viewports|
+        viewports.each_with_index do |viewport, index|
           rank = (index + 1).to_s
           viewport.update_attributes_that_reference_rank(rank)
         end
       end
     end
 
-    def viewport_collections
-      [top_mobile_viewports, top_tablet_viewports, top_desktop_viewports]
+    def viewport_types
+      [viewports[:mobile], viewports[:tablet], viewports[:desktop]]
     end
   end
 end
