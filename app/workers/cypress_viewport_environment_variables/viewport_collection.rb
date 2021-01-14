896825362f727e7e2c4cea1ef8bcfc874d12cf3c
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-13 19:17:24 -0500

    refector ViewportCollection so it takes two google analytics reports and calculates the percentage of users for each viewport

--- app/workers/cypress_viewport_environment_variables/viewport_collection.rb
+++ app/workers/cypress_viewport_environment_variables/viewport_collection.rb
@@ -4,23 +4,29 @@ module CypressViewportEnvironmentVariables
     NUMBER_OF_TOP_VIEWPORTS = 5
     DEVICES = "" # create device lookup table based on width and height
 
-    attr_reader :start_date, :end_date, :google_analytics_report,
-                 :top_mobile_viewports, :top_tablet_viewports, :top_desktop_viewports
+    attr_reader :start_date, :end_date, :total_users,
+                :google_analytics_viewport_report, :top_mobile_viewports,
+                :top_tablet_viewports, :top_desktop_viewports
     
-    def initialize(start_date, end_date, report)
+    def initialize(start_date:, end_date:, user_report:, viewport_report:)
       @top_mobile_viewports = []
       @top_tablet_viewports = []
       @top_desktop_viewports = []
       @start_date = start_date
       @end_date = end_date
-      @google_analytics_report = report
-      parse_report
+      @total_users = parse_user_report_for_total_users(user_report)
+      @google_analytics_viewport_report = viewport_report
+      parse_viewport_report
     end
 
     private
 
-    def parse_report
-      google_analytics_report.data.rows.each do |row|
+    def parse_user_report_for_total_users(user_report)
+      user_report.data.totals.first.values.first.to_f
+    end
+
+    def parse_viewport_report
+      google_analytics_viewport_report.data.rows.each do |row|
         device = row.dimensions.first
 
         case device
@@ -50,17 +56,22 @@ module CypressViewportEnvironmentVariables
       dimensions = row.dimensions
       device, resolution = dimensions[0], dimensions[1]
       width, height = resolution.split('x')
+      number_of_users = row.metrics.first.values.first.to_f
 
       {
         list: "VA Top #{device.capitalize} Viewports",
         rank: nil,
         devicesWithViewport: DEVICES,
-        percentTraffic: nil, # oops, i forgot to get this from GA!
-        percentTrafficPeriod: "#{start_date}--#{end_date}",
-        viewportPreset: "va-top-#{device.capitalize}-",
+        percentTraffic: "#{calculate_percentage_of_users_who_use_viewport(number_of_users)}%",
+        percentTrafficPeriod: "from #{start_date} to #{end_date}",
+        viewportPreset: "va-top-#{device}-",
         width: width,
         height: height
       }
+    end
+
+    def calculate_percentage_of_users_who_use_viewport(number_of_users)
+      (number_of_users / total_users * 100).round(2)
     end
   end
 end
