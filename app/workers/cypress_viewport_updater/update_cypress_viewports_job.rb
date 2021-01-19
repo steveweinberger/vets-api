6f21848755628268d6c62281e4db99d5e54be226
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-19 18:12:08 -0500

    add commented-out code that connects to github, grabs the files that need to be updated, creates a new feature branch, uploads the updated files, and submits a pr

--- app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
+++ app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
@@ -21,16 +21,54 @@ module CypressViewportUpdater
                                                      viewport_report: reports.viewport_report)
       create_updated_cypress_json_file(viewport_collection)
       create_updated_viewport_preset_js_file(viewport_collection)
-      client = CypressViewportUpdater::GithubApp
-      # TO-DO
-      # GITHUB API
-      # get cypress.json
-      # get viewportPreset.js
-      # update files (see above)
-      # create new git branch
-      # commit cypress.json
-      # commit viewportPreset.js
-      # submit pr
+
+      # # The following code uses a temporary personal access token from my account
+      # # It connects to a forked version of vets-website
+      # # It grabs the files I need to update, creates a new feature brach,
+      # #   uploads the files to the new branch, and submits a pr
+      # # Next step: Refactor the code.
+
+      # data = YAML.safe_load(File.open('config/settings.local.yml'))
+      # ACCESS_TOKEN = data['github_cypress_viewport_updater_bot']['access_token']
+      # client = Octokit::Client.new(:access_token => ACCESS_TOKEN)
+
+      # cypress_json_path = 'config/cypress.json'
+      # cypress_json_sha = client.content('holdenhinkle/vets-website', path: cypress_json_path).sha
+      # cypress_json_content = client.content('holdenhinkle/vets-website', path: cypress_json_path, accept: 'application/vnd.github.V3.raw')
+
+      # viewport_preset_js_path= 'src/platform/testing/e2e/cypress/support/commands/viewportPreset.js'
+      # viewport_preset_js_sha = client.content('holdenhinkle/vets-website', path: viewport_preset_js_path).sha
+      # viewport_preset_js_content = client.content('holdenhinkle/vets-website', path: viewport_preset_js_path, accept: 'application/vnd.github.V3.raw')
+      
+      # date = Date.today
+      # date_string = "#{date.mon}_#{date.day}_#{date.year}"
+      # ref = "heads/#{date_string}_update_cypress_viewport_data"
+
+      # # CREATE BRANCH
+      # # get sha of the last commit to base-off
+      # sha = client.ref('holdenhinkle/vets-website', 'heads/master').object.sha
+      # branch = client.create_ref('holdenhinkle/vets-website', ref, sha)
+
+      # # UPDATE FILE
+      # feature_branch = "#{date_string}_update_cypress_viewport_data"
+      # client.update_contents("holdenhinkle/vets-website",
+      #            cypress_json_path,
+      #            "This is commit #1",
+      #            cypress_json_sha,
+      #            "cypress.json content goes here..",
+      #            :branch => feature_branch)
+
+      # client.update_contents("holdenhinkle/vets-website",
+      #            viewport_preset_js_path,
+      #            "This is commit #2",
+      #            viewport_preset_js_sha,
+      #            "viewportPreset.js content goes here...",
+      #            :branch => feature_branch)
+      
+      # client.create_pull_request("holdenhinkle/vets-website",
+      #   "master",
+      #   feature_branch,
+      #   "Update Cypress Viewport Data")
     end
 
     private
