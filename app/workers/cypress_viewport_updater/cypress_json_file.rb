009304d5f0b0e0c46bef0652d3778532022a5793
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-20 16:48:18 -0500

    create new CypressJsonFile and ViewportPresetJs classes that subclass from ExistingGithubFile and have an update method to update the contents of each file

---
+++ app/workers/cypress_viewport_updater/cypress_json_file.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module CypressViewportUpdater
+  class CypressJsonFile < ExistingGithubFile
+    def initialize
+      super(github_path: 'config/cypress.json',
+            local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/cypress.json',
+            local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/cypress.json',
+            name: 'cypress.json')
+    end
+
+    def update(collection)
+      create_local_current_file
+      file_as_hash = JSON.parse(File.read(local_current_file_path))
+      update_viewports(hash: file_as_hash, collection: collection)
+      File.delete(local_updated_file_path) if File.exist?(local_updated_file_path)
+      File.write(local_updated_file_path, JSON.pretty_generate(JSON.parse(file_as_hash.to_json)))
+      self
+    end
+
+    private
+
+    def update_viewports(hash:, collection:)
+      hash['env']['vaTopMobileViewports'] = collection.viewports[:mobile]
+      hash['env']['vaTopTabletViewports'] = collection.viewports[:tablet]
+      hash['env']['vaTopDesktopViewports'] = collection.viewports[:desktop]
+    end
+  end
+end
---
+++ app/workers/cypress_viewport_updater/viewport_preset_js_file.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module CypressViewportUpdater
+  class ViewportPresetJsFile < ExistingGithubFile
+    def initialize
+      super(github_path: 'src/platform/testing/e2e/cypress/support/commands/viewportPreset.js',
+            local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/viewportPreset.js',
+            local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/viewportPreset.js',
+            name: 'viewportPreset.js')
+    end
+
+    def update(collection)
+      create_local_current_file
+      new_file = create_updated_viewport_preset_js_file
+
+      File.open(local_current_file_path, 'r').each do |line|
+        if /va-top-(mobile|tablet|desktop)-\d+/.match(line)
+          if /va-top-(mobile|tablet|desktop)-1/.match(line)
+            print_viewport_presets(file: new_file,
+                                   line: line,
+                                   collection: collection)
+          end
+        else
+          new_file.print line
+        end
+      end
+
+      new_file.close
+      self
+    end
+
+    private
+
+    def create_updated_viewport_preset_js_file
+      File.delete(local_updated_file_path) if File.exist?(local_updated_file_path)
+      File.new(local_updated_file_path, 'w')
+    end
+
+    def print_viewport_presets(file:, line:, collection:)
+      viewport_type = /(mobile|tablet|desktop)/.match(line)[0]
+      viewports = collection.viewports[viewport_type.to_sym]
+
+      viewports.each_with_index do |viewport, i|
+        width = viewport.width
+        height = viewport.height
+        preset = "  'va-top-#{viewport_type}-#{i + 1}': { width: #{width}, height: #{height} },\n"
+        file.print preset
+      end
+    end
+  end
+end
