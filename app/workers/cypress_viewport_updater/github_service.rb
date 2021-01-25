9f7c9f43432fdd0aa8a4a65abb37ca5bb0e85b5f
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-24 22:16:50 -0500

    make client a private instance variable

--- app/workers/cypress_viewport_updater/github_service.rb
+++ app/workers/cypress_viewport_updater/github_service.rb
@@ -6,26 +6,27 @@ module CypressViewportUpdater
     ACCESS_TOKEN = data['github_cypress_viewport_updater_bot']['access_token']
     REPO = 'holdenhinkle/vets-website'
 
-    attr_reader :client, :feature_branch_name
+    attr_reader :feature_branch_name
 
     def initialize
       @client = Octokit::Client.new(access_token: ACCESS_TOKEN)
     end
 
     def get_content(file)
-      file.sha = client.content(REPO, path: file.github_path).sha
-      file.raw_content = client.content(REPO, path: file.github_path, accept: 'application/vnd.github.V3.raw')
+      file.sha = @client.content(REPO, path: file.github_path).sha
+      file.raw_content = @client.content(REPO, path: file.github_path, accept: 'application/vnd.github.V3.raw')
+      self
     end
 
     def create_branch
       set_feature_branch_name
       ref = "heads/#{feature_branch_name}"
-      sha = client.ref(REPO, 'heads/master').object.sha
-      client.create_ref(REPO, ref, sha)
+      sha = @client.ref(REPO, 'heads/master').object.sha
+      @client.create_ref(REPO, ref, sha)
     end
 
     def update_content(file:, content:)
-      client.update_content(REPO,
+      @client.update_content(REPO,
                             file.github_path,
                             "update #{file.name}",
                             file.sha,
@@ -34,7 +35,7 @@ module CypressViewportUpdater
     end
 
     def submit_pr
-      client.create_pull_request(REPO,
+      @client.create_pull_request(REPO,
                                  'master',
                                  feature_branch_name,
                                  pr_title,
