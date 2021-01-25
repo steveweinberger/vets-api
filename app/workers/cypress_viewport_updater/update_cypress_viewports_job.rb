9e93f194a7acc1b86713e2860f9073ce8647427e
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-24 22:26:02 -0500

    refactor UpdateCypressViewportsJob#perform when writing unit tests

--- app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
+++ app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
@@ -17,8 +17,8 @@ module CypressViewportUpdater
                   .create(viewport_report: analytics.viewport_report)
 
       github = CypressViewportUpdater::GithubService.new
-      cypress_json_file = CypressViewportUpdater::CypressJsonFile.new
-      viewport_preset_js_file = CypressViewportUpdater::ViewportPresetJsFile.new
+      cypress_json_file = create_cypress_json_file
+      viewport_preset_js_file = create_viewport_preset_js_file
       create_local_directories
       github.get_content(cypress_json_file)
       github.get_content(viewport_preset_js_file)
@@ -33,6 +33,24 @@ module CypressViewportUpdater
     end
 
     private
+
+    def create_cypress_json_file
+      CypressViewportUpdater::CypressJsonFile.new(
+        github_path: 'config/cypress.json',
+        local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/cypress.json',
+        local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/cypress.json',
+        name: 'cypress.json'
+      )
+    end
+
+    def create_viewport_preset_js_file
+      CypressViewportUpdater::ViewportPresetJsFile.new(
+        github_path: 'src/platform/testing/e2e/cypress/support/commands/viewportPreset.js',
+        local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/viewportPreset.js',
+        local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/viewportPreset.js',
+        name: 'viewportPreset.js'
+      )
+    end
 
     def create_local_directories
       current_files_directory = 'app/workers/cypress_viewport_updater/current_files'
