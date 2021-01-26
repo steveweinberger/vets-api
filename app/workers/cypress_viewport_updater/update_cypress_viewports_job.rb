6cb4261c92e3e06be818ca6da9542eefff7a29f8
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-26 13:52:45 -0500

    update UpdateCypressViewportsJob so files are stored in memory instead of on disk (don't delete and create directories, files)

--- app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
+++ app/workers/cypress_viewport_updater/update_cypress_viewports_job.rb
@@ -17,50 +17,19 @@ module CypressViewportUpdater
                   .create(viewport_report: analytics.viewport_report)
 
       github = CypressViewportUpdater::GithubService.new
-      cypress_json_file = create_cypress_json_file
-      viewport_preset_js_file = create_viewport_preset_js_file
-      create_local_directories
-      github.get_content(cypress_json_file)
-      github.get_content(viewport_preset_js_file)
+      cypress_json_file = CypressViewportUpdater::CypressJsonFile.new
+      viewport_preset_js_file = CypressViewportUpdater::ViewportPresetJsFile.new
+      github.get_content(file: cypress_json_file)
+      github.get_content(file: viewport_preset_js_file)
       github.create_branch
 
       [cypress_json_file, viewport_preset_js_file].each do |file|
-        content = file.update(viewports).content
-        binding.pry
-        github.update_content(file: file, content: content)
+        file.update(viewports: viewports)
+        github.update_content(file: file)
       end
 
       github.submit_pr
       self
-    end
-
-    private
-
-    def create_cypress_json_file
-      CypressViewportUpdater::CypressJsonFile.new(
-        github_path: 'config/cypress.json',
-        local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/cypress.json',
-        local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/cypress.json',
-        name: 'cypress.json'
-      )
-    end
-
-    def create_viewport_preset_js_file
-      CypressViewportUpdater::ViewportPresetJsFile.new(
-        github_path: 'src/platform/testing/e2e/cypress/support/commands/viewportPreset.js',
-        local_current_file_path: 'app/workers/cypress_viewport_updater/current_files/viewportPreset.js',
-        local_updated_file_path: 'app/workers/cypress_viewport_updater/updated_files/viewportPreset.js',
-        name: 'viewportPreset.js'
-      )
-    end
-
-    def create_local_directories
-      current_files_directory = 'app/workers/cypress_viewport_updater/current_files'
-      updated_files_directory = 'app/workers/cypress_viewport_updater/updated_files'
-
-      [current_files_directory, updated_files_directory].each do |dirname|
-        FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
-      end
     end
   end
 end
