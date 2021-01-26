49ffc0a8cd695de7f0f13c234dee341254557846
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-26 08:30:34 -0500

    change vcr cassette name in 'after #request_report is called' block

--- spec/jobs/cypress_viewport_updater/google_analytics_reports_spec.rb
+++ spec/jobs/cypress_viewport_updater/google_analytics_reports_spec.rb
@@ -39,65 +39,65 @@ RSpec.describe CypressViewportUpdater::GoogleAnalyticsReports do
 
   describe 'after #request_report is called' do
     before do
-      VCR.use_cassette('cypress_viewport_updater/google_analytics_after_request_report') do
-        @after_request_report = CypressViewportUpdater::GoogleAnalyticsReports
-                                .new
-                                .request_reports
+      VCR.use_cassette('cypress_viewport_updater/google_analytics_request_report') do
+        @request_report = CypressViewportUpdater::GoogleAnalyticsReports
+                          .new
+                          .request_reports
       end
     end
 
     describe '#user_report' do
       it 'returns a report object' do
-        report = @after_request_report.user_report
+        report = @request_report.user_report
         klass = 'Google::Apis::AnalyticsreportingV4::Report'
         expect(report.class.to_s).to eq(klass)
       end
 
       it 'returns a report object with metric name ga:users' do
-        metric_name = @after_request_report.user_report.column_header.metric_header.metric_header_entries.first.name
+        metric_name = @request_report.user_report.column_header.metric_header.metric_header_entries.first.name
         expect(metric_name).to eq('ga:users')
       end
 
       it 'returns a report object that has a row count of 1' do
-        row_count = @after_request_report.user_report.data.row_count
+        row_count = @request_report.user_report.data.row_count
         expect(row_count).to eq(1)
       end
 
       it 'returns a report object that has a total number of users' do
-        total_users = @after_request_report.user_report.data.rows.first.metrics.first.values.first
+        total_users = @request_report.user_report.data.rows.first.metrics.first.values.first
         expect(total_users).to eq('14356199')
       end
     end
 
     describe '#viewport_report' do
       it 'returns a report object' do
-        report = @after_request_report.viewport_report
+        report = @request_report.viewport_report
         klass = 'Google::Apis::AnalyticsreportingV4::Report'
         expect(report.class.to_s).to eq(klass)
       end
 
       it 'returns a report object with metric name ga:users' do
-        metric_name = @after_request_report.viewport_report.column_header.metric_header.metric_header_entries.first.name
+        metric_name = @request_report.viewport_report.column_header.metric_header.metric_header_entries.first.name
         expect(metric_name).to eq('ga:users')
       end
 
       it 'returns a report object with primary dimension ga:deviceCategory' do
-        primary_dimension = @after_request_report.viewport_report.column_header.dimensions.first
+        primary_dimension = @request_report.viewport_report.column_header.dimensions.first
         expect(primary_dimension).to eq('ga:deviceCategory')
       end
 
       it 'returns a report object with secondary dimension ga:screenResolution' do
-        secondary_dimension = @after_request_report.viewport_report.column_header.dimensions.second
+        secondary_dimension = @request_report.viewport_report.column_header.dimensions.second
         expect(secondary_dimension).to eq('ga:screenResolution')
       end
 
       it 'returns a report object that has 100 results' do
-        results_total = @after_request_report.viewport_report.data.rows.count
+        results_total = @request_report.viewport_report.data.rows.count
         expect(results_total).to eq(100)
       end
 
       it 'returns a report object that includes mobile devices' do
-        includes_mobile = @after_request_report.viewport_report.data.rows.any? do |row|
+        includes_mobile = @request_report.viewport_report.data.rows.any? do |row|
           row.dimensions.first == 'mobile'
         end
 
@@ -105,7 +105,7 @@ RSpec.describe CypressViewportUpdater::GoogleAnalyticsReports do
       end
 
       it 'returns a report object that includes tablet devices' do
-        includes_tablet = @after_request_report.viewport_report.data.rows.any? do |row|
+        includes_tablet = @request_report.viewport_report.data.rows.any? do |row|
           row.dimensions.first == 'tablet'
         end
 
@@ -113,7 +113,7 @@ RSpec.describe CypressViewportUpdater::GoogleAnalyticsReports do
       end
 
       it 'returns a report object that includes desktop devices' do
-        includes_desktop = @after_request_report.viewport_report.data.rows.any? do |row|
+        includes_desktop = @request_report.viewport_report.data.rows.any? do |row|
           row.dimensions.first == 'desktop'
         end
 
@@ -121,7 +121,7 @@ RSpec.describe CypressViewportUpdater::GoogleAnalyticsReports do
       end
 
       it 'returns a report object that includes screen resolutions' do
-        includes_screen_resolutions = @after_request_report.viewport_report.data.rows.any? do |row|
+        includes_screen_resolutions = @request_report.viewport_report.data.rows.any? do |row|
           /[\d]+x[\d]+/.match(row.dimensions.second)
         end
 
