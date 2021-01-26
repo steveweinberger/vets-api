3c7b7ee16dc499ba4c922cb8e1985cc60fe02522
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-26 13:29:59 -0500

    refactor CypressJsonFile so the raw data from github and the updated data is stored in memory instead of on disk (don't use the file system)

--- app/workers/cypress_viewport_updater/cypress_json_file.rb
+++ app/workers/cypress_viewport_updater/cypress_json_file.rb
@@ -2,28 +2,17 @@
 
 module CypressViewportUpdater
   class CypressJsonFile < ExistingGithubFile
-    def initialize(github_path:, local_current_file_path:, local_updated_file_path:, name:)
-      super(github_path: github_path,
-            local_current_file_path: local_current_file_path,
-            local_updated_file_path: local_updated_file_path,
-            name: name)
+    def initialize
+      super(github_path: 'config/cypress.json', name: 'cypress.json')
     end
 
-    def update(viewports)
-      create_local_current_file
-      file_as_hash = JSON.parse(File.read(local_current_file_path))
-      update_viewports(hash: file_as_hash, viewports: viewports)
-      File.delete(local_updated_file_path) if File.exist?(local_updated_file_path)
-      File.write(local_updated_file_path, JSON.pretty_generate(JSON.parse(file_as_hash.to_json)))
-      self
-    end
-
-    private
-
-    def update_viewports(hash:, viewports:)
+    def update(viewports:)
+      hash = JSON.parse(raw_content)
       hash['env']['vaTopMobileViewports'] = viewports.mobile
       hash['env']['vaTopTabletViewports'] = viewports.tablet
       hash['env']['vaTopDesktopViewports'] = viewports.desktop
+      self.updated_content = JSON.pretty_generate(JSON.parse(hash.to_json))
+      self
     end
   end
 end
