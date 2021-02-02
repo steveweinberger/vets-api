e4318f884f4c21343627caf3bef8a5ffb65504cc
Dillo Raju <draju@governmentcio.com>
2021-02-02 14:41:42 -0500

    Questionnaire Manager WIP

--- modules/health_quest/app/services/health_quest/questionnaire_manager/factory.rb
+++ modules/health_quest/app/services/health_quest/questionnaire_manager/factory.rb
@@ -3,13 +3,16 @@
 module HealthQuest
   module QuestionnaireManager
     class Factory
-      attr_reader :appointments,
+      attr_reader :aggregated_data,
+                  :appointments,
                   :patient,
+                  :questionnaires,
+                  :questionnaire_responses,
+                  :save_in_progress_data,
                   :appointment_service,
                   :patient_service,
                   :questionnaire_service,
                   :questionnaire_response_service,
-                  :questionnaires,
                   :user
 
       def self.manufacture(user)
@@ -17,6 +20,7 @@ module HealthQuest
       end
 
       def initialize(user)
+        @aggregated_data = default_aggregated_data
         @user = user
         @appointment_service = AppointmentService.new(user)
         @patient_service = PatientGeneratedData::Patient::Factory.manufacture(user)
@@ -25,21 +29,27 @@ module HealthQuest
       end
 
       def all
-        get_or_create_patient
+        get_patient
         get_appointments
         get_questionnaires
         get_questionnaire_responses
+        get_sip_data
+        compose
       end
 
       def get_appointments
         @appointments = appointment_service.get_appointments(three_months_ago, one_year_from_now)
       end
 
-      def get_or_create_patient; end
+      def get_patient; end
 
       def get_questionnaires; end
 
       def get_questionnaire_responses; end
+
+      def get_sip_data; end
+
+      def compose; end
 
       private
 
@@ -49,6 +59,10 @@ module HealthQuest
 
       def one_year_from_now
         1.year.from_now.in_time_zone
+      end
+
+      def default_aggregated_data
+        { data: [] }
       end
     end
   end
