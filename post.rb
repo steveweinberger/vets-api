d07625597ed3e9c51d5c6f4cf09008de175108d4
Cris Shupp <cristopher.shupp@libertyits.com>
2021-05-25 13:16:19 -0400

    initial commit

--- post.rb
+++ post.rb
@@ -10,7 +10,9 @@ https.use_ssl = true
 request = Net::HTTP::Post.new(url)
 request["apikey"] = "mulgyIRUpqY8SeJoM89e3t2iZucbZVSH"
 request["Cookie"] = "TS016f4012=01c8917e48a0e98bbf7907f5de00bc23de968d681c87098af0bf1fefdd2cb92236a39e685c4708b9d1c5a7e938b5def5925f50c882"
-form_data = [['metadata', File.open(metadata)],['content', File.open(pdf)],['attachment1', File.open(pdf)],['attachment2', File.open(pdf)]]
+form_data = [['metadata', 'application/json', File.open(metadata)],
+             ['content', 'application/pdf', File.open(pdf)],['attachment1', File.open(pdf)],
+             ['attachment2','application/pdf', File.open(pdf)]]
 request.set_form form_data, 'multipart/form-data'
 t1 = Time.now
 response = https.request(request)
