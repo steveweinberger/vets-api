754a9aca11c5c1d0865a22b323aad932dcb22538
Cris Shupp <cristopher.shupp@libertyits.com>
2021-05-25 12:44:32 -0400

    initial commit

--- speed.rb
+++ speed.rb
@@ -3,7 +3,7 @@ require 'faraday'
 
 #curl -v -L -X POST 'https://dev-api.va.gov/services/vba_documents/v2/uploads/submit' -F 'metadata="{\"veteranFirstName\": \"Matsumoto\",\"veteranLastName\": \"Test\",\"fileNumber\": \"012345678\",\"zipCode\": \"97202\",\"source\": \"MyVSO\",\"docType\": \"21-22\"}";type=application/json' -F 'content=@"1.pdf"' -F 'attachment1=@"v1.pdf"' -F 'attachment2=@"v1.pdf"' -F 'attachment3=@"v1.pdf"'
 url = 'https://dev-api.va.gov/services/vba_documents/v2/uploads'
-pdf = Faraday::UploadIO.new('./v1.pdf', 'application/pdf')
+pdf = Faraday::UploadIO.new('./1.pdf', 'application/pdf')
 metadata = Faraday::UploadIO.new('./valid_metadata.json', 'application/json')
 
 conn = Faraday.new(url: url) do |faraday|
