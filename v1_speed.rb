8723f21993a0c16e7c45c85f83c9e564b356513d
Cris Shupp <cristopher.shupp@libertyits.com>
2021-05-25 16:28:24 -0400

    initial commit

--- v1_speed.rb
+++ v1_speed.rb
@@ -1,40 +1,51 @@
 require 'json'
+pdf_file = ARGV[1].to_s
+metadata = ARGV[0].to_s
+num_times = ARGV[2].to_i
+pids = []
 
-command =  <<EOF
+command = <<EOF
 curl -v -L -X POST 'https://dev-api.va.gov/services/vba_documents/v1/uploads/' -H 'apikey: mulgyIRUpqY8SeJoM89e3t2iZucbZVSH'
 EOF
 
+num_times.times do |i|
+  pids << fork do
 
-curl = `#{command}`
-hash = JSON.parse curl
-url = hash['data']['attributes']['location']
-puts '------------------------------------------'
-puts url
-puts '------------------------------------------'
-require 'faraday'
-URL = url
+    curl = `#{command}`
+    hash = JSON.parse curl
+    url = hash['data']['attributes']['location']
+    puts '------------------------------------------'
+    puts url
+    puts '------------------------------------------'
+    require 'faraday'
+    URL = url
 
-pdf_file = ARGV[1].to_s
-puts "I found #{pdf_file}, #{File.exists?(pdf_file)}"
+    puts "I found #{pdf_file}, #{File.exists?(pdf_file)}"
 
-pdf1 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
-pdf2 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
-pdf3 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
-metadata = Faraday::UploadIO.new(ARGV[0].to_s, 'application/json')
+    pdf1 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+    pdf2 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+    pdf3 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+    metadata = Faraday::UploadIO.new(metadata.to_s, 'application/json')
 
-conn = Faraday.new(url: URL) do |faraday|
-  faraday.request :multipart
-  faraday.request :url_encoded
-  faraday.adapter Faraday.default_adapter
+    conn = Faraday.new(url: URL) do |faraday|
+      faraday.request :multipart
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
 
+    end
+
+    payload = {metadata: metadata, content: pdf1, attachment1: pdf2, attachement2: pdf3}
+
+    t1 = Time.now
+    puts "Starting put...."
+    response = conn.put('', payload)
+    t2 = Time.now
+    puts response.body
+    puts response.status
+    File.write("body_#{i}", response.body+ "\n")
+    File.write("status_#{i}", response.status.to_s + "\n")
+    File.write("time_#{i}", "I took #{t2 - t1} seconds\n")  end
 end
-
-payload = { metadata: metadata, content: pdf1, attachment1: pdf2, attachement2: pdf3}
-
-t1 = Time.now
-puts "Starting put...."
-response = conn.put('', payload)
-t2 = Time.now
-puts response.body
-puts response.status
-print "I took #{t2- t1} seconds"
+puts "Waiting"
+pids.each { |pid| Process.waitpid(pid) } # wait for my children to completeputs "Summary:"
+puts "Done!"
