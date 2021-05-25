4af95472c887df60b34e23688fc4e55d8423e84a
Cris Shupp <cristopher.shupp@libertyits.com>
2021-05-25 12:29:57 -0400

    initial commit

--- speed.rb
+++ speed.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/ruby
-puts "Hi Cris and Mark!"
+require 'faraday'
+
+#curl -v -L -X POST 'https://dev-api.va.gov/services/vba_documents/v2/uploads/submit' -F 'metadata="{\"veteranFirstName\": \"Matsumoto\",\"veteranLastName\": \"Test\",\"fileNumber\": \"012345678\",\"zipCode\": \"97202\",\"source\": \"MyVSO\",\"docType\": \"21-22\"}";type=application/json' -F 'content=@"1.pdf"' -F 'attachment1=@"v1.pdf"' -F 'attachment2=@"v1.pdf"' -F 'attachment3=@"v1.pdf"'
+url = 'https://dev-api.va.gov/services/vba_documents/v2/uploads/submit'
+found = File.exists? './v1.pdf'
+puts "Hi Cris and Mark! I found the file #{found}"
