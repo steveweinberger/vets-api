a34ab25ddfcabe9701f9feb24f46517f53d2d0bb
Cris Shupp <cristopher.shupp@libertyits.com>
2021-05-25 13:37:15 -0400

    initial commit

--- speed.rb
+++ speed.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 require 'faraday'
-URL = 'https://dev-api.va.gov/services/vba_documents/v2/uploads/submit'
+URL = 'https://dev-api.va.gov/services/vba_documents/v2/uploads/'
 
 #curl -v -L -X POST 'https://dev-api.va.gov/services/vba_documents/v2/uploads/submit' -F 'metadata="{\"veteranFirstName\": \"Matsumoto\",\"veteranLastName\": \"Test\",\"fileNumber\": \"012345678\",\"zipCode\": \"97202\",\"source\": \"MyVSO\",\"docType\": \"21-22\"}";type=application/json' -F 'content=@"1.pdf"' -F 'attachment1=@"v1.pdf"' -F 'attachment2=@"v1.pdf"' -F 'attachment3=@"v1.pdf"'
-pdf_file = './1.pdf'
+pdf_file = ARGV[1].to_s
 puts "I found #{pdf_file}, #{File.exists?(pdf_file)}"
-pdf = Faraday::UploadIO.new(pdf_file, 'application/pdf')
-metadata = Faraday::UploadIO.new('./valid_metadata.json', 'application/json')
+pdf1 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+pdf2 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+pdf3 = Faraday::UploadIO.new(pdf_file, 'application/pdf')
+metadata = Faraday::UploadIO.new(ARGV[0].to_s, 'application/json')
 
 conn = Faraday.new(url: URL) do |faraday|
   faraday.request :multipart
@@ -14,11 +16,11 @@ conn = Faraday.new(url: URL) do |faraday|
   faraday.adapter Faraday.default_adapter
 end
 
-payload = { metadata: metadata, content: pdf, attachment1: pdf, attachement2: pdf}
+payload = { metadata: metadata, content: pdf1, attachment1: pdf2, attachement2: pdf3}
 
 t1 = Time.now
 puts "Starting post...."
-response = conn.post('/', payload)
+response = conn.post('/submit', payload)
 t2 = Time.now
 puts response.body
 puts response.status
