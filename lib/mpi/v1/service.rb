23ec56d58815553354dae4970d294e0b68cf8384
Lihan Li <frankieteardrop@gmail.com>
2021-08-05 16:21:55 -0700

    add mpi v1 service spec stub

--- lib/mpi/v1/service.rb
+++ lib/mpi/v1/service.rb
@@ -1,6 +1,9 @@
 module MPI
   module V1
     class Service
+      def initialize(user)
+        @user = user
+      end
     end
   end
 end
---
+++ spec/lib/mpi/v1/service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+require 'mpi/service'
+
+describe MPI::V1::Service do
+end
