6e2a712da172169c3e00ae517fa9bb82f4b68ad1
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-09 11:41:21 -0600

    fix

--- config/initializers/file_utils_monkey_patch.rb
+++ config/initializers/file_utils_monkey_patch.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 require 'fileutils'
-# rubocop:disable Naming/ClassAndModuleCamelCase
-module FileUtils
-  class Entry_
-    def copy_file(dest)
-      File.open(path) do |s|
-        File.open(dest, 'wb', s.stat.mode) do |f|
-          f.chmod f.lstat.mode
-          IO.copy_stream(s, f)
-        end
+FileUtils.Entry_.module_eval do
+  def copy_file(dest)
+    File.open(path) do |s|
+      File.open(dest, 'wb', s.stat.mode) do |f|
+        IO.copy_stream(s, f)
+        f.chmod f.lstat.mode
       end
     end
   end
 end
-# rubocop:enable Naming/ClassAndModuleCamelCase
