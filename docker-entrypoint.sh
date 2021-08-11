5354c26b658f0461451c106e96ad21d63ced1ca4
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-11 09:46:12 -0600

    sudo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-rmdir /usr/local/bundle
+sudo rmdir /usr/local/bundle
 # bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
 
 exec "$@"
