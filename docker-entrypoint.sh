3bb8acdb2d7476908b87ede8ad3f4e0cd552fe62
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-11 13:54:48 -0600

    bundle check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ unset BUNDLE_BIN
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-# bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
+bundle check
 
 exec "$@"
 
