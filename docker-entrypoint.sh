2dc10b5bc4601b59d71ac5a206358abbeb1ffb43
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-11 08:24:42 -0600

    install bundler

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
 
-# bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
+gem install bundler
+bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin"
 
 exec "$@"
 
