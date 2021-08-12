fb24c443a6875ec8e959b6389c4a36043a8b8949
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-12 09:10:10 -0600

    update user

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 unset BUNDLE_PATH
 unset BUNDLE_BIN
 
+# sh -c "sudo chown -R vets-api /srv/vets-api/src"
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
