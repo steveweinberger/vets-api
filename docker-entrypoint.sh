0a0820e57bf634fb94e3e8e5ecb415fbfe17ac65
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-13 13:41:13 -0600

    set bin

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@
 # if you make major alteration here, please check that usage as well
 
 bundle config --local path $PWD/vendor/bundle
+bundle config --local bin $PWD/vendor/bundle/ruby/2.6.0/bin
 vendor/bundle/ruby/2.6.0
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4
 
