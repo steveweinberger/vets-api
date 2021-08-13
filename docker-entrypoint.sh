c8a8464c5d3babfdeca22d29a6e47a43e6c67936
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-13 12:36:09 -0600

    ls

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-
+ls
 bundle config path vendor/bundle
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4
 
