ff2955affd94a3d80b858dc4eb1582a7429ecce0
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-13 12:12:17 -0600

    mount lib

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
 
-ls /usr/local/bundle
-gem pristine ffi
+
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4
 
 exec "$@"
