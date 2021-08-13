a1900e75da744db4824ad4f669031a11cc2a2315
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-13 14:52:46 -0600

    pristine

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
 ls /usr/local/bundle/gems/ffi-1.15.1/lib
+gem pristine ffi
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4
 
 exec "$@"
