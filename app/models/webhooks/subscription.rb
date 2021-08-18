9943ad89bb44980f3b8a2f68f6548d7aedf94d30
KevinCranmer <cranmerkevin39@gmail.com>
2021-08-18 11:51:32 -0500

    Cleaning up VBADocuments V2 Serializer

--- app/models/webhooks/subscription.rb
+++ app/models/webhooks/subscription.rb
@@ -22,21 +22,6 @@ module Webhooks
       retrieve_event_urls(sql, api_name, consumer_id, event)
     end
 
-    def self.get_observers_by_guid(api_name:, consumer_id:, api_guid:)
-      uuid_regex = /^[a-f0-9]{8}-[a-f0-9]{4}-[0-5][a-f0-9]{3}-[089ab][a-f0-9]{3}-[a-f0-9]{12}$/i
-      return [] unless uuid_regex.match?(consumer_id) && uuid_regex.match?(api_guid)
-
-      sql = "
-        select a.events -> 'subscriptions' as api_consumer_subscriptions
-        from webhooks_subscriptions a
-        where a.api_name = $1
-        and a.consumer_id = $2
-        and a.events -> 'subscriptions' is not null
-        and a.api_guid = $3
-      "
-      retrieve_observers_by_guid(sql, api_name, consumer_id, api_guid)
-    end
-
     def self.retrieve_event_urls(sql, *args)
       result = ActiveRecord::Base.connection_pool.with_connection do |c|
         c.raw_connection.exec_params(sql, args).to_a
@@ -46,19 +31,6 @@ module Webhooks
         result = JSON.parse(result.first['event_urls']).uniq
       end
       result
-    end
-
-    def self.retrieve_observers_by_guid(sql, *args)
-      result = ActiveRecord::Base.connection_pool.with_connection do |c|
-        c.raw_connection.exec_params(sql, args).to_a
-      end
-
-      if result.any?
-        observers = result.first['api_consumer_subscriptions']
-        JSON.parse(observers)
-      else
-        []
-      end
     end
   end
 end
