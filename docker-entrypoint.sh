4e0251a4824edcfbff32b6cbc0efd28b30d328ca
Riley Anderson <5150r.d.anderson@gmail.com>
2021-08-20 07:25:21 -0600

    apt install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 # note this logic is duplicated in the Dockerfile for prod builds,
 # if you make major alteration here, please check that usage as well
-curl -LO http://archive.ubuntu.com/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-8_amd64.deb
-dpkg -i libffi6_3.2.1-8_amd64.deb
+curl -LO http://mirrors.kernel.org/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-8_amd64.deb
+apt install ./libffi6_3.2.1-8_amd64.deb
 
 bundle check || bundle install --binstubs="${BUNDLE_APP_CONFIG}/bin" --jobs=4 --deployment
 
