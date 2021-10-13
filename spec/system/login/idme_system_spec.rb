b40a2c21cae09fc58835fa51308ce0071af5eed5
Josh Scanish <josh.scanish@oddball.io>
2021-10-13 13:08:03 -0400

    Test that user can log in using ID.me

--- spec/system/login/idme_system_spec.rb
+++ spec/system/login/idme_system_spec.rb
@@ -1,15 +1,24 @@
 require "rails_helper"
 
-RSpec.describe 'ID.me login', type: :system do
-  before do
-    driven_by :selenium_chrome
-    VCR.turn_off!
-    Capybara.app_host = 'https://staging.va.gov'
-    Capybara.run_server = false # don't start Rack
-  end
+if ENV['LOGIN_SYSTEM_TESTS']
+  RSpec.describe 'ID.me login', type: :system do
+    before do
+      driven_by :selenium_chrome
+      VCR.turn_off!
+      Capybara.app_host = 'https://staging.va.gov'
+      Capybara.run_server = false # don't start Rack
+    end
 
-  it 'can log in an LOA1 user' do
-    visit '/'
-    click_button 'Sign in'
+    it 'can log in an LOA1 user' do
+      visit '/'
+      click_button 'Sign in'
+      click_button 'Sign in with ID.me'
+      sleep(5)
+      fill_in 'Email', with: ENV['EMAIL']
+      fill_in 'Password', with: ENV['PASSWORD']
+      click_button 'Sign in to ID.me'
+      click_button 'Continue'
+      click_button 'Continue'
+    end
   end
 end
