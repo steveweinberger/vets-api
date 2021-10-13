79fddbb3b9dd92436649fde2a8f18241890f3839
Josh Scanish <josh.scanish@oddball.io>
2021-10-13 12:12:18 -0400

    Turn off vcr and set capybara host

--- spec/system/login/idme_system_spec.rb
+++ spec/system/login/idme_system_spec.rb
@@ -3,8 +3,13 @@ require "rails_helper"
 RSpec.describe 'ID.me login', type: :system do
   before do
     driven_by :selenium_chrome
+    VCR.turn_off!
+    Capybara.app_host = 'https://staging.va.gov'
+    Capybara.run_server = false # don't start Rack
   end
 
   it 'can log in an LOA1 user' do
+    visit '/'
+    click_button 'Sign in'
   end
 end
