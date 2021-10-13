84dab17585aa056249375cc9e182b8fa5b46891c
Holden Hinkle <holden@holdenhinkle.com>
2021-10-13 13:45:00 -0400

    make /oauth/logout route

--- modules/test_user_dashboard/config/routes.rb
+++ modules/test_user_dashboard/config/routes.rb
@@ -2,6 +2,7 @@
 
 TestUserDashboard::Engine.routes.draw do
   get '/oauth/is_authorized', to: 'oauth#is_authorized'
+  get '/oauth/logout', to: 'oauth#logout'
   resources :oauth, only: [:index]
   resources :tud_accounts, only: [:index, :update]
 end
