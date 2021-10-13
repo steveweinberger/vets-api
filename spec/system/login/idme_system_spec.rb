fe6ab2af9245d1271d34dd7214a0640e104d8e8f
Josh Scanish <josh.scanish@oddball.io>
2021-10-13 17:39:09 -0400

    Finish test

--- spec/system/login/idme_system_spec.rb
+++ spec/system/login/idme_system_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 if ENV['LOGIN_SYSTEM_TESTS']
   RSpec.describe 'ID.me login', type: :system do
     before do
-      driven_by :selenium_chrome
+      driven_by :selenium_chrome_headless
       Capybara.server = :puma
       Capybara.app_host = 'https://staging.va.gov'
       Capybara.run_server = false # don't start Rack
-      Capybara.default_max_wait_time = 8
+      Capybara.default_max_wait_time = 15
       VCR.turn_off!
     end
 
-    it 'can log in an LOA1 user' do
+    it 'can log in an LOA3 user' do
       visit '/'
       click_button 'Sign in'
       click_button 'Sign in with ID.me'
@@ -20,6 +20,9 @@ if ENV['LOGIN_SYSTEM_TESTS']
       click_button 'Sign in to ID.me'
       click_button 'Continue'
       click_button 'Continue'
+
+      expect(page).to have_content('My Health')
+      expect(page).not_to have_content('Sign in')
     end
   end
 end
