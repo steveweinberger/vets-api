15e31e9f1128a566f8cb824b5e762cf78cd3d562
Holden Hinkle <holden@holdenhinkle.com>
2021-10-13 13:44:29 -0400

    make /oauth/is_authorized route

--- modules/test_user_dashboard/config/routes.rb
+++ modules/test_user_dashboard/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 TestUserDashboard::Engine.routes.draw do
+  get '/oauth/is_authorized', to: 'oauth#is_authorized'
   resources :oauth, only: [:index]
   resources :tud_accounts, only: [:index, :update]
 end
