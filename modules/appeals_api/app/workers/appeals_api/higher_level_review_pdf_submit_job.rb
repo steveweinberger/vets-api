31262017cff6bdb22a2c9a4e7f9e3700ace80c97
Nathan Wright <nathan.wright@oddball.io>
2021-10-13 20:10:29 -0600

    Rename hlr variables to be generic

--- modules/appeals_api/app/workers/appeals_api/higher_level_review_pdf_submit_job.rb
+++ modules/appeals_api/app/workers/appeals_api/higher_level_review_pdf_submit_job.rb
@@ -18,18 +18,18 @@ module AppealsApi
     # Retry for ~7 days
     sidekiq_options retry: 20
 
-    def perform(higher_level_review_id, version = 'V1', handler:, appeal_klass:)
-      higher_level_review = handler.new(appeal_klass.find(higher_level_review_id))
+    def perform(appeal_id, version = 'V1', handler:, appeal_klass:)
+      appeal = handler.new(appeal_klass.find(appeal_id))
 
       begin
-        stamped_pdf = AppealsApi::PdfConstruction::Generator.new(higher_level_review, version: version).generate
-        higher_level_review.update_status!(status: 'submitting')
-        upload_to_central_mail(higher_level_review, stamped_pdf)
+        stamped_pdf = AppealsApi::PdfConstruction::Generator.new(appeal, version: version).generate
+        appeal.update_status!(status: 'submitting')
+        upload_to_central_mail(appeal, stamped_pdf)
         File.delete(stamped_pdf) if File.exist?(stamped_pdf)
       rescue AppealsApi::UploadError => e
-        handle_upload_error(higher_level_review, e)
+        handle_upload_error(appeal, e)
       rescue => e
-        higher_level_review.update_status!(status: 'error', code: e.class.to_s, detail: e.message)
+        appeal.update_status!(status: 'error', code: e.class.to_s, detail: e.message)
         Rails.logger.error("#{self.class} error: #{e}")
         raise
       end
@@ -46,41 +46,41 @@ module AppealsApi
 
     private
 
-    def upload_to_central_mail(higher_level_review, pdf_path)
-      metadata = higher_level_review.metadata(pdf_path)
+    def upload_to_central_mail(appeal, pdf_path)
+      metadata = appeal.metadata(pdf_path)
       body = { 'metadata' => metadata.to_json,
-               'document' => to_faraday_upload(pdf_path, higher_level_review.pdf_file_name) }
-      process_response(CentralMail::Service.new.upload(body), higher_level_review, metadata)
+               'document' => to_faraday_upload(pdf_path, appeal.pdf_file_name) }
+      process_response(CentralMail::Service.new.upload(body), appeal, metadata)
     end
 
-    def process_response(response, higher_level_review, metadata)
+    def process_response(response, appeal, metadata)
       if response.success? || response.body.match?(NON_FAILING_ERROR_REGEX)
-        higher_level_review.update_status!(status: 'submitted')
-        log_submission(higher_level_review, metadata)
+        appeal.update_status!(status: 'submitted')
+        log_submission(appeal, metadata)
       else
         map_error(response.status, response.body, AppealsApi::UploadError)
       end
     end
 
-    def log_upload_error(higher_level_review, e)
-      Rails.logger.error("#{higher_level_review.class.to_s.gsub('::', ' ')}: Submission failure",
-                         'source' => higher_level_review.consumer_name,
-                         'consumer_id' => higher_level_review.consumer_id,
-                         'consumer_username' => higher_level_review.consumer_name,
-                         'uuid' => higher_level_review.id,
+    def log_upload_error(appeal, e)
+      Rails.logger.error("#{appeal.class.to_s.gsub('::', ' ')}: Submission failure",
+                         'source' => appeal.consumer_name,
+                         'consumer_id' => appeal.consumer_id,
+                         'consumer_username' => appeal.consumer_name,
+                         'uuid' => appeal.id,
                          'code' => e.code,
                          'detail' => e.detail)
     end
 
-    def handle_upload_error(higher_level_review, e)
-      log_upload_error(higher_level_review, e)
-      higher_level_review.update(status: 'error', code: e.code, detail: e.detail)
+    def handle_upload_error(appeal, e)
+      log_upload_error(appeal, e)
+      appeal.update(status: 'error', code: e.code, detail: e.detail)
 
       if e.code == 'DOC201' || e.code == 'DOC202'
         notify(
           {
             'class' => self.class.name,
-            'args' => [higher_level_review.id],
+            'args' => [appeal.id],
             'error_class' => e.code,
             'error_message' => e.detail,
             'failed_at' => Time.zone.now
