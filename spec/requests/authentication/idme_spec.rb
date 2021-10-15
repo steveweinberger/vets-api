8f1ef609c4f0958de6e52535951430d7675d6277
Josh Scanish <josh.scanish@oddball.io>
2021-10-15 17:20:57 -0400

    Start writing test

--- spec/requests/authentication/idme_spec.rb
+++ spec/requests/authentication/idme_spec.rb
@@ -2,5 +2,19 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Authenticating through ID.me', type: :request do
+RSpec.describe 'Authenticating through ID.me', type: :request, js: true do
+  context 'loa1 user' do
+    it 'will authenticate user successfully' do
+      get '/v1/sessions/idme/new'
+      expect(response).to have_http_status(:ok)
+
+      doc = Nokogiri::HTML(body)
+      form = doc.at_css('[id="saml-form"]')
+
+      url = form.attributes['action'].value
+      form_inputs = form.children.select { |child| child.name == 'input' }
+
+      post form.attributes['action'].value, params:
+    end
+  end
 end
