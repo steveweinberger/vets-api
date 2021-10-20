e301147dc1e33177c566a243f4eeed50a7600f4c
Josh Scanish <josh.scanish@oddball.io>
2021-10-20 17:37:36 -0400

    Start ds logon tests

---
+++ spec/system/login/dslogon_system_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'authentication_system_helper'
+
+if ENV['LOGIN_SYSTEM_TESTS']
+  RSpec.describe 'DS Logon login', type: :system do
+    it 'can login a DS Logon Premium user' do
+      visit '/'
+      click_button 'Sign in'
+      click_button 'Sign in with DS Logon'
+
+      fill_in 'userName', with: ENV['DS_PREMIUM_USERNAME']
+      find_field('password-clear').click
+      fill_in 'password', with: ENV['PASSWORD']
+      click_button 'Login'
+
+      find(:xpath, "//img[@alt='tiger-relaxing.png']").click
+      click_button 'Continue'
+    end
+  end
+end
