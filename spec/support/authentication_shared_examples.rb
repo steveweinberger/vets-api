91d93632c2a545fc9f9409d51d46457f5f0f006d
Josh Scanish <josh.scanish@oddball.io>
2021-10-25 12:32:02 -0400

    Use shared examples to test multiple ds logon credentials

--- spec/support/authentication_shared_examples.rb
+++ spec/support/authentication_shared_examples.rb
@@ -1,34 +1,57 @@
 # frozen_string_literal: true
 
 module AuthenticationSharedExamples
+  def expect_logged_in_home_screen
+    expect(page).to have_content('My Health')
+    expect(page).not_to have_content('Sign in')
+  end
+
   RSpec.shared_examples 'logs in LOA1 ID.me user' do |email|
-    it 'logs in loa1 user' do
+    it 'logs in idme loa1 user' do
       visit '/'
       click_button 'Sign in'
       click_button 'Sign in with ID.me'
       fill_in 'Email', with: email
-      fill_in 'Password', with: ENV['LOA1_PASSWORD']
+      fill_in 'Password', with: ENV['IDME_LOA1_PASSWORD']
       click_button 'Sign in to ID.me'
       click_button 'Continue'
 
-      expect(page).to have_content('My Health')
-      expect(page).not_to have_content('Sign in')
+      expect_logged_in_home_screen
     end
   end
 
   RSpec.shared_examples 'logs in LOA3 ID.me user' do |email|
-    it 'logs in loa3 user' do
+    it 'logs in idme loa3 user' do
       visit '/'
       click_button 'Sign in'
       click_button 'Sign in with ID.me'
       fill_in 'Email', with: email
-      fill_in 'Password', with: ENV['LOA3_PASSWORD']
+      fill_in 'Password', with: ENV['IDME_LOA3_PASSWORD']
       click_button 'Sign in to ID.me'
       click_button 'Continue'
       click_button 'Continue'
 
-      expect(page).to have_content('My Health')
-      expect(page).not_to have_content('Sign in')
+      expect_logged_in_home_screen
+    end
+  end
+
+  RSpec.shared_examples 'logs in DS Logon LOA2 user' do |username|
+    it 'logs in dslogon loa2 user' do
+      visit '/'
+      click_button 'Sign in'
+      click_button 'Sign in with DS Logon'
+
+      fill_in 'userName', with: username
+      find_field('password-clear').click
+      fill_in 'password', with: ENV['DSLOGON_LOA2_PASSWORD']
+      click_button 'Login'
+
+      find(:xpath, "//img[@alt='tiger-relaxing.png']").click
+      click_button 'Continue'
+
+      click_button 'Continue' if has_content?('Contact Information Verification')
+
+      expect_logged_in_home_screen
     end
   end
 end
--- spec/system/login/dslogon_system_spec.rb
+++ spec/system/login/dslogon_system_spec.rb
@@ -4,22 +4,10 @@ require 'authentication_system_helper'
 
 if ENV['LOGIN_SYSTEM_TESTS']
   RSpec.describe 'DS Logon login', type: :system do
-    it 'can login a DS Logon Premium user' do
-      visit '/'
-      click_button 'Sign in'
-      click_button 'Sign in with DS Logon'
-
-      fill_in 'userName', with: ENV['DS_PREMIUM_USERNAME']
-      find_field('password-clear').click
-      fill_in 'password', with: ENV['PASSWORD']
-      click_button 'Login'
-
-      find(:xpath, "//img[@alt='tiger-relaxing.png']").click
-      click_button 'Continue'
-
-      click_button 'Continue' if has_content?('Contact Information Verification')
-
-      expect_logged_in_home_screen
+    context 'LOA2 premium' do
+      include_examples 'logs in DS Logon LOA2 user', 'ace.a.mcghee1'
+      include_examples 'logs in DS Logon LOA2 user', 'ardeshir.t.caulder1'
+      include_examples 'logs in DS Logon LOA2 user', 'marti.c.ortizrivera1'
     end
   end
 end
