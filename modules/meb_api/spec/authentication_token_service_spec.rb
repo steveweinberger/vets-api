4dfa3e8c78ae6b5855dac0e59fe66619ec5709e4
bradbergeron-us <bradley.bergeron@accenturefederal.com>
2021-10-27 14:09:49 -0400

    Boilerplate jwt setup

---
+++ modules/meb_api/spec/authentication_token_service_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+Rspec.describe AuthenticationTokenService do
+  describe '.call' do
+    let(:token) { described_class.call }
+
+    it 'returns an authentication token' do
+      decoded_token = JWT.decode(token,
+                                 described_class::RSA_PUBLIC,
+                                 true,
+                                 { algorithm: described_class::ALGORITHM_TYPE }
+      )
+
+      expect(decoded_token).to eq([
+                                    {
+                                      'sub' => '1234567890',
+                                      'name' => 'John Doe',
+                                      'admin' => true,
+                                      'realm_access' => {
+                                        'roles' => [
+                                          'dgi_user'
+                                        ]
+                                      }
+                                    }, header_fields= {'alg' => 'RS256'}
+                                    
+                                    ])
+    end
+  end
+end
