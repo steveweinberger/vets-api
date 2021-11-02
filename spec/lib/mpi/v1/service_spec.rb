401aba7d054f790bf41684bd6059e31b9adddf90
Lihan Li <frankieteardrop@gmail.com>
2021-11-02 13:58:45 -0700

    lint

--- spec/lib/mpi/v1/service_spec.rb
+++ spec/lib/mpi/v1/service_spec.rb
@@ -286,10 +286,10 @@ describe MPI::V1::Service do
         it 'raises a invalid request error', :aggregate_failures do
           invalid_xml = File.read('spec/support/mpi/find_candidate_invalid_request.xml')
           allow_any_instance_of(MasterPersonIndex::Service).to receive(:create_profile_message).and_return(invalid_xml)
-          expect(subject).to receive(:log_exception_to_sentry)
+          expect(service).to receive(:log_exception_to_sentry)
 
           VCR.use_cassette('mpi/find_candidate/invalid') do
-            response = subject.find_profile(user)
+            response = service.find_profile(user)
             server_error_502_expectations_for(response)
           end
         end
