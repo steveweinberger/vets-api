5e6547adfde629a707943f5c201498e40e713244
Julia Allen <julia.l.allen@gsa.gov>
2021-11-05 17:47:08 -0700

    Commit current settings (some set up for local testing that should be changed back)

--- app/workers/disability_compensation_fast_track_job.rb
+++ app/workers/disability_compensation_fast_track_job.rb
@@ -11,13 +11,22 @@ class DisabilityCompensationFastTrackJob
   def perform(form526_submission_id)
     submission = Form526Submission.find(form526_submission_id)
     icn = Account.where(idme_uuid: submission.user_uuid).first.icn
-
+    #temporary below
+    # icn = 2000163
     client = Lighthouse::ClinicalHealth::Client.new
-    condition_response = client.get_condition(icn)
+    # TODO: rescue !=200 responses with an appropriate action
+    condition_response = client.get_conditions(icn)
+    return unless is_hypertension?(condition_response)
+    # TODO: rescue !=200 responses with an appropriate action
+    observations_response = client.get_observations(icn)
     pdf_body = generate_pdf(condition_response)
 
     client = EVSS::DocumentsService.new(submission.auth_headers)
     client.upload(pdf_body, create_document_data(upload_data))
+  end
+
+  def is_hypertension?(condition_response)
+    condition_response.body['entry'].each { |e| return true if e['resource']['code']['text'].downcase == 'hypertension' }
   end
 
   def generate_pdf(_condition_response)
