ef8b7e6e867bb88baa59bd91401e164da9fec630
Josh Scanish <josh.scanish@oddball.io>
2021-11-09 12:59:59 -0500

    No need for shared examples in id.me file

--- spec/support/authentication/inbound_shared_examples.rb
+++ spec/support/authentication/inbound_shared_examples.rb
@@ -8,28 +8,6 @@ module Authentication
       find(:xpath, "//img[@alt='VHIC Self-Service logo']").click
     end
 
-    RSpec.shared_examples 'logs in inbound ID.me user from eauth' do |email, password|
-      it 'logs in inbound idme user from eauth' do
-        navigate_through_eauth
-
-        click_on 'Sign in with ID.me'
-        click_on 'Accept'
-
-        fill_in 'Email', with: email
-        fill_in 'Password', with: password
-        click_button 'Sign in to ID.me'
-
-        click_button 'Continue' if has_content?('COMPLETE YOUR SIGN IN')
-        click_button 'Continue' if has_content?('COMPLETE YOUR SIGN IN')
-
-        # Finding this element ensures that we wait for redirect before visiting staging.va.gov
-        find('a', text: 'Veteran Health Identification Card (VHIC)')
-
-        visit '/'
-        expect_user_logged_in
-      end
-    end
-
     RSpec.shared_examples 'logs in inbound DS Logon user from eauth' do |username, password|
       it 'logs in inbound dslogon user from eauth' do
         navigate_through_eauth
--- spec/system/login/idme_system_spec.rb
+++ spec/system/login/idme_system_spec.rb
@@ -17,7 +17,25 @@ if ENV['LOGIN_SYSTEM_TESTS']
 
     context 'inbound' do
       context 'LOA3' do
-        include_examples 'logs in inbound ID.me user from eauth', 'ssoissoetesting+SPO1@gmail.com', ENV['IDME_LOA3_PASSWORD']
+        it 'logs in inbound idme user from eauth' do
+          navigate_through_eauth
+
+          click_on 'Sign in with ID.me'
+          click_on 'Accept'
+
+          fill_in 'Email', with: 'vets.gov.user+24@gmail.com'
+          fill_in 'Password', with: ENV['IDME_LOA3_PASSWORD']
+          click_button 'Sign in to ID.me'
+
+          click_button 'Continue' if has_content?('COMPLETE YOUR SIGN IN')
+          click_button 'Continue' if has_content?('COMPLETE YOUR SIGN IN')
+
+          # Finding this element ensures that we wait for redirect before visiting staging.va.gov
+          find('a', text: 'Veteran Health Identification Card (VHIC)')
+
+          visit '/'
+          expect_user_logged_in
+        end
       end
     end
   end
