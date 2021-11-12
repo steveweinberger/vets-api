956c9ec998e28f6a20d4a7dd76116bccd91e1ece
cadibemma <chika.adibemma@adhocteam.us>
2021-11-12 11:52:57 -0500

    Added file for manufacturer map

---
+++ modules/mobile/config/initializers/cdc_cvx_manufacturer_map.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Mobile::CDC_CVX_MANUFACTURER_MAP = {
+  207 => 'Moderna US, Inc.',
+  208 => 'Pfizer, Inc',
+  210 => 'AstraZeneca',
+  211 => 'Novavax, Inc.',
+  212 => 'Janssen',
+  217 => 'Pfizer, Inc',
+  218 => 'Pfizer, Inc',
+  219 => 'Pfizer, Inc',
+  510 => 'Sinopharm-Biotech',
+  511 => 'Sinovac'
+}.freeze
