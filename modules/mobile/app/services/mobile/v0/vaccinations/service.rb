118d3132a5232a7b8da5b865f1430cdba1858c46
Kris Pethtel <kris.pethtel@gmail.com>
2021-11-11 23:24:31 -0500

    1930: add vaccine group names

---
+++ modules/mobile/app/services/mobile/v0/vaccinations/service.rb
@@ -0,0 +1,42 @@
+module Mobile
+  module V0
+    module Vaccinations
+      # Service that connects to CDC's vaccinations XML table
+      # https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=cvx
+      #
+      class Service
+        doc = Nokogiri::XML(URI.open('https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=cvx')) do |config|
+          config.strict.noblanks
+        end
+
+        matches = {
+          current: {},
+          new: {}
+        }
+
+        doc.root.children.each do |node|
+          ary = node.children.to_a
+          index = ary.find_index {|c| c.name == "Name" && c.text == "CVX Code" }
+          cvx_code = ary[index + 1].text.strip
+          group_name_index = ary.find_index {|c| c.name == "Name" && c.text == "Short Description" }
+          group_name = ary[group_name_index + 1].text.strip
+          if Mobile::CDC_CVX_CODE_MAP.keys.include?(cvx_code.to_i)
+            matches[:current][cvx_code.to_i] = group_name
+          else
+            matches[:new][cvx_code.to_i] = group_name
+          end
+        end
+        existing = matches[:current].sort.to_h
+        incoming = matches[:new].sort.to_h
+
+        puts "EXISTING"
+        pp existing
+        puts "INCOMING"
+        pp incoming
+
+        puts "UNIQUES"
+        pp (existing.values + incoming.values).uniq.sort
+      end
+    end
+  end
+end
