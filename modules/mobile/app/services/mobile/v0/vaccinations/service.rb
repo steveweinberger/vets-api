4c91249acdef9a00381da1de827ac8b695fea9d3
Kris Pethtel <kris.pethtel@gmail.com>
2021-11-16 14:05:52 -0500

    adding initial setting of manufacturer from cdc xml file

--- modules/mobile/app/services/mobile/v0/vaccinations/service.rb
+++ modules/mobile/app/services/mobile/v0/vaccinations/service.rb
@@ -10,6 +10,9 @@ module Mobile
         doc = Nokogiri::XML(URI.open('https://www2.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=vax2vg')) do |config|
           config.strict.noblanks
         end
+        manufacturer_doc = Nokogiri::XML(URI.open('https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=tradename')) do |config|
+          config.strict.noblanks
+        end
 
         doc.root.children.each do |node|
           cvx_code = nil
@@ -17,7 +20,7 @@ module Mobile
           node.children.each_slice(2) do |(name, value)|
             break if cvx_code && group_name
 
-            case name.text
+            case name.text.strip
             when 'CVXCode'
               cvx_code = value.text.strip
             when 'Vaccine Group Name'
@@ -25,8 +28,41 @@ module Mobile
             end
           end
 
+          manufacturer = nil
+          if group_name == "COVID-19"
+            manufacturer_doc.root.children.each do |manufacturer_node|
+              break if manufacturer
+
+              _manufacturer = nil
+              match = nil
+              manufacturer_node.children.each_slice(2) do |(manufacturer_name_row, manufacturer_value_row)|
+                break if match == false
+
+                # this logic needs to move
+                if _manufacturer && match
+                  manufacturer = _manufacturer
+                  break
+                end
+
+                if manufacturer_name_row.text.strip == "Manufacturer"
+                  _manufacturer = manufacturer_value_row.text.strip
+                  puts _manufacturer
+                end
+                if manufacturer_name_row.text.strip == "CVXCode"
+                  if manufacturer_value_row.text.strip == cvx_code
+                    match = true
+                  else
+                    match = false
+                  end
+                end
+              end
+            end
+          end
+
           vaccine = Vaccine.find_or_create_by(cvx_code: cvx_code)
-          vaccine.update(group_name: group_name) unless vaccine.group_name == group_name
+          if vaccine.group_name != group_name || vaccine.manufacturer != manufacturer
+            vaccine.update(group_name: group_name, manufacturer: manufacturer)
+          end
         end
       end
     end
