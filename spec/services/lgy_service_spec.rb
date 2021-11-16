691cac90eee8a2da4e3aa3982c60bad73d8da94c
Kevin Musiorski <kevin.musiorski@gcio.com>
2021-11-15 22:26:00 -0600

    LgyService   #new     creates a new object of type LgyService

--- spec/services/lgy_service_spec.rb
+++ spec/services/lgy_service_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 describe LgyService do
-
+  describe '#new' do
+    it 'creates a new object of type LgyService' do
+      expect(subject).to be_instance_of(LgyService)
+    end
+  end
 end
