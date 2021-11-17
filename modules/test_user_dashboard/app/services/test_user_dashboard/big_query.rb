088b951f735f4abc84456afcdda858a6b8decdda
Holden Hinkle <holden@holdenhinkle.com>
2021-11-17 17:20:54 -0500

    revert TestUserDashboard::BigQuery

--- modules/test_user_dashboard/app/services/test_user_dashboard/big_query.rb
+++ modules/test_user_dashboard/app/services/test_user_dashboard/big_query.rb
@@ -18,43 +18,17 @@ module TestUserDashboard
       log_exception_to_sentry(e)
     end
 
-    def select_all(table_name:, where: nil, order_by: nil, limit: nil)
-      clauses = [where, order_by, limit]
-                .reject(&:nil?)
-                .join(' ')
-
-      sql = if clauses.empty?
-              "SELECT * FROM `#{PROJECT}.#{DATASET}.#{table_name}`"
-            else
-              "SELECT * FROM `#{PROJECT}.#{DATASET}.#{table_name}` " \
-                "#{clauses}"
-            end
-
-      query(sql)
-    end
-
-    def update(table_name:, set:, where: nil)
-      clauses = [set, where].reject(&:nil?).join(' ')
-
-      sql = "UPDATE `#{PROJECT}.#{DATASET}.#{table_name}` #{clauses}"
-
-      query(sql)
-    end
-
     # BigQuery requires a row indentifier in DELETE FROM statements
-    def delete_from(table_name:, where:)
-      sql = "DELETE FROM `#{PROJECT}.#{DATASET}.#{table_name}` #{where}"
+    def delete_from(table_name:, row_identifier: 'account_uuid')
+      sql = "DELETE FROM `#{PROJECT}.#{DATASET}.#{table_name}` " \
+            "WHERE #{row_identifier} IS NOT NULL"
 
-      query(sql)
+      bigquery.query sql do |config|
+        config.location = 'US'
+      end
+    rescue => e
+      log_exception_to_sentry(e)
     end
-
-    # # BigQuery requires a row indentifier in DELETE FROM statements
-    # def delete_from(table_name:, row_identifier: 'account_uuid')
-    #   sql = "DELETE FROM `#{PROJECT}.#{DATASET}.#{table_name}` " \
-    #         "WHERE #{row_identifier} IS NOT NULL"
-
-    #   query(sql)
-    # end
 
     def insert_into(table_name:, rows:)
       # rubocop:disable Rails/SkipsModelValidations
@@ -72,14 +46,6 @@ module TestUserDashboard
 
     def table(table_name:)
       dataset.table table_name, skip_lookup: true
-    end
-
-    def query(sql)
-      bigquery.query sql do |config|
-        config.location = 'US'
-      end
-    rescue => e
-      log_exception_to_sentry(e)
     end
   end
 end
