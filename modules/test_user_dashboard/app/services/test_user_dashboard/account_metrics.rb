f285af979c47da3b0bed932082d661e011938be5
Holden Hinkle <holden@holdenhinkle.com>
2021-11-17 15:29:59 -0500

    refactor TestUserDashboard::AccountMetrics

--- modules/test_user_dashboard/app/services/test_user_dashboard/account_metrics.rb
+++ modules/test_user_dashboard/app/services/test_user_dashboard/account_metrics.rb
@@ -2,30 +2,23 @@
 
 module TestUserDashboard
   class AccountMetrics
-    TABLE = 'tud_accounts_usage'
-
-    attr_reader :tud_account
+    attr_reader :tud_account, :record
 
     def initialize(user)
       @tud_account = TudAccount.find_by(account_uuid: user.account_uuid)
+      @record = last_tud_account_checkout_record
     end
 
     def checkin(checkin_time:, is_manual_checkin: false)
       return unless tud_account
 
-      row = { checkin_time: checkin_time, is_manual_checkin: is_manual_checkin }
-
-      TestUserDashboard::BigQuery.new.insert_into(table_name: TABLE, rows: [row])
+      if record.present? && record.checkin_time.nil?
+        record.update(checkin_time: checkin_time, is_manual_checkin: is_manual_checkin)
+      end
     end
 
     def checkout
       return unless tud_account
-
-      record = TestUserDashboard::TudAccountCheckout
-        .where(account_uuid: tud_account.account_uuid)
-        .last
-
-      binding.pry
 
       record.update(has_checkin_error: true) if record.present? && record.checkin_time.nil?
 
@@ -33,6 +26,12 @@ module TestUserDashboard
         account_uuid: tud_account.account_uuid,
         checkout_time: Time.now.utc
       )
+    end
+
+    private
+
+    def last_tud_account_checkout_record
+      TestUserDashboard::TudAccountCheckout.where(account_uuid: tud_account.account_uuid).last
     end
   end
 end
