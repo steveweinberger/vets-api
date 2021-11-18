9da9ec2c7f3148a9a3f9d3f98c82f5dd5ebb4c28
Kris Pethtel <kris.pethtel@gmail.com>
2021-11-18 13:07:46 -0500

    style changes

--- modules/mobile/app/workers/mobile/v0/vaccines_updater_job.rb
+++ modules/mobile/app/workers/mobile/v0/vaccines_updater_job.rb
@@ -5,13 +5,13 @@ module Mobile
     class VaccinesUpdaterJob
       include Sidekiq::Worker
 
-      GROUP_NAME_XML = 'https://www2.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=vax2vg'
-      MANUFACTURER_XML = 'https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=tradename'
+      GROUP_NAME_URL = 'https://www2.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=vax2vg'
+      MANUFACTURER_URL = 'https://www2a.cdc.gov/vaccines/iis/iisstandards/XML.asp?rpt=tradename'
 
       def perform
-        group_name_data.root.children.each do |node|
-          cvx_code = find_value_from(node, 'CVXCode')
-          group_name = find_value_from(node, 'Vaccine Group Name')
+        group_name_xml.root.children.each do |node|
+          cvx_code = find_value(node, 'CVXCode')
+          group_name = find_value(node, 'Vaccine Group Name')
           manufacturer = group_name == 'COVID-19' ? find_manufacturer(cvx_code) : nil
 
           vaccine = Mobile::V0::Vaccine.find_or_create_by(cvx_code: cvx_code)
@@ -23,19 +23,19 @@ module Mobile
 
       private
 
-      def group_name_data
-        @group_name_data ||= Nokogiri::XML(URI.parse(GROUP_NAME_XML).open) do |config|
+      def group_name_xml
+        @group_name_xml ||= Nokogiri::XML(URI.parse(GROUP_NAME_URL).open) do |config|
           config.strict.noblanks
         end
       end
 
-      def manufacturer_data
-        @manufacturer_data ||= Nokogiri::XML(URI.parse(MANUFACTURER_XML).open) do |config|
+      def manufacturer_xml
+        @manufacturer_xml ||= Nokogiri::XML(URI.parse(MANUFACTURER_URL).open) do |config|
           config.strict.noblanks
         end
       end
 
-      def find_value_from(node, property_name)
+      def find_value(node, property_name)
         node.children.each_slice(2) do |(name, value)|
           return value.text.strip if name.text.strip == property_name
         end
@@ -43,11 +43,11 @@ module Mobile
       end
 
       def find_manufacturer(cvx_code)
-        manufacturer_data.root.children.each do |node|
-          current_node_cvx = find_value_from(node, 'CVXCode')
+        manufacturer_xml.root.children.each do |node|
+          current_node_cvx = find_value(node, 'CVXCode')
           next unless current_node_cvx == cvx_code
 
-          return find_value_from(node, 'Manufacturer')
+          return find_value(node, 'Manufacturer')
         end
         nil
       end
