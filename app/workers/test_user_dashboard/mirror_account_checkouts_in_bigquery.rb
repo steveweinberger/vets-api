873a044907bf6b686d2988ae09e2c36ea4061c41
Holden Hinkle <holden@holdenhinkle.com>
2021-11-18 13:19:48 -0500

    create job to mirror tud_account_checkouts table in bigquery

---
+++ app/workers/test_user_dashboard/mirror_account_checkouts_in_bigquery.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module TestUserDashboard
+  class MirrorAccountCheckoutsInBigQuery
+    include Sidekiq::Worker
+
+    TUD_ACCOUNTS_USAGE_TABLE = 'tud_accounts_usage'
+
+    def perform
+      mirror_account_checkouts_in_bigquery
+    end
+
+    private
+
+    def mirror_account_checkouts_in_bigquery
+      client = TestUserDashboard::BigQuery.new
+      client.delete_from(table_name: TUD_ACCOUNTS_USAGE_TABLE)
+      client.insert_into(table_name: TUD_ACCOUNTS_USAGE_TABLE, rows: checkouts)
+    end
+
+    def checkouts
+      TestUserDashboard::TudAccountCheckout.all.each.with_object([]) do |account, rows|
+        rows << account.attributes.reject { |attr, _| attr == 'id' }
+      end
+    end
+  end
+end
