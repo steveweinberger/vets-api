2ebcf6f6f91c7370e26f3112a3c08b24db574021
Tadhg O'Higgins <tadhg@tadhg.com>
2021-11-18 12:35:35 -0800

    Early checkin.

--- app/workers/disability_compensation_fast_track_job.rb
+++ app/workers/disability_compensation_fast_track_job.rb
@@ -9,10 +9,10 @@ class DisabilityCompensationFastTrackJob
   sidekiq_options retry: 14
 
   def perform(form526_submission_id)
-    submission = Form526Submission.find(form526_submission_id)
-    icn = Account.where(idme_uuid: submission.user_uuid).first.icn
+    # submission = Form526Submission.find(form526_submission_id)
+    # icn = Account.where(idme_uuid: submission.user_uuid).first.icn
     #temporary below
-    # icn = 2000163
+    icn = 2000163
     client = Lighthouse::ClinicalHealth::Client.new
     # TODO: rescue !=200 responses with an appropriate action
     condition_response = client.get_conditions(icn)
@@ -20,10 +20,15 @@ class DisabilityCompensationFastTrackJob
     # TODO: rescue !=200 responses with an appropriate action
     observations_response = client.get_observations(icn)
     pdf_body = generate_pdf(condition_response)
+    binding.pry
+    results = HypertensionObservationData.new(observations_response).transform
+    # entries = observations_response.body.dig('entry')
+    # results = entries.map {|entry| transform_entry(entry)}
 
-    client = EVSS::DocumentsService.new(submission.auth_headers)
-    client.upload(pdf_body, create_document_data(upload_data))
+    # client = EVSS::DocumentsService.new(submission.auth_headers)
+    # client.upload(pdf_body, create_document_data(upload_data))
   end
+
 
   def is_hypertension?(condition_response)
     condition_response.body['entry'].each { |e| return true if e['resource']['code']['text'].downcase == 'hypertension' }
@@ -48,3 +53,147 @@ class DisabilityCompensationFastTrackJob
     )
   end
 end
+
+# What should the DisabilityCompensationFastTrackJob class do, as opposed to helper class(es).
+# 1. Get the conditions data about the claim.
+# 2. If conditions data does not match hypertension, do nothing.
+# 3. Otherwise: call LH for more data to get BP and medication data; parse that data; shape that data; generate a PDF from that data; attach PDF to EVSS; attach special issue (RRD) to EVSS claim; submit EVSS claim. (Not in that order necessarily)
+# Helper classes to:
+# - parse LH API call, shape, return it.
+
+class HypertensionObservationData
+  attr_accessor :response
+
+  def initialize(response)
+    response = @response
+  end
+
+  def transform
+    entries = response.body.dig('entry')
+    entries.map {|entry| transform_entry(entry)}
+  end
+
+  private
+
+  def pick(keys, hash)
+    result = hash.select {|k, v| keys.include? k}
+    return result
+  end
+
+  def transform_entry(raw_entry)
+    # TODO: DO we need to verify LOINC code 85354-9 here as well?
+    # TODO: I'm using issued here over effectiveDateTime, is that correct?
+    entry = pick(["issued", "component", "performer"], raw_entry["resource"])
+    result = {"issued": entry["issued"]}
+    practitioner_hash = get_display_hash_from_performer("Practitioner", entry)
+    organization_hash = get_display_hash_from_performer("Organization", entry)
+    bp_hash = get_bp_readings_from_entry(entry)
+    result = result.merge(practitioner_hash, organization_hash, bp_hash)
+    return result
+  end
+
+  def get_display_hash_from_performer(term, entry)
+    result = {}
+    performer = entry["performer"].select {|item| item["reference"].include? term}.first
+    if performer.present?
+      result[term.downcase.to_sym] = performer["display"]
+    end
+    return result
+
+  end
+
+  def get_bp_readings_from_entry(entry)
+    
+    result = {}
+    # Each component should contain a BP pair, so after filtering there should only be one reading of each type:
+    systolic = filter_components_by_code("8480-6", entry["component"]).first
+    diastolic = filter_components_by_code("8462-4", entry["component"]).first
+
+    # systolic = nil
+    # diastolic = nil
+    # for component in entry["component"]
+
+      # for coding in component["code"]["coding"]
+#        if ["8480-6", "8462-4"].include? coding["code"]
+#          reading = {
+#            "code": coding["code"],
+#            "unit": component["valueQuantity"]["unit"],
+#            "value": component["valueQuantity"]["value"],
+#            "display": coding["display"]
+#          }
+#          if coding["code"] == "8480-6"
+#            systolic = reading
+#          elsif coding["code"] == "8462-4"
+#            diastolic = reading
+#          end
+#        else
+#          # TODO: not clear that we need this, because there could conceivably
+#          # be codes--like codes from other systems--that are present in
+#          # addition to the ones we want, and we probably don't need to error
+#          # on that.
+#          raise "not systolic or diastolic"
+#        end
+
+#        if coding["code"] == "8480-6"
+#          systolic = {
+#            "code": coding["code"],
+#            "unit": component["valueQuantity"]["unit"],
+#            "value": component["valueQuantity"]["value"],
+#            "display": coding["display"]
+#          }
+#        elsif coding["code"] == "8462-4"
+#          diastolic = {
+#            "code": coding["code"],
+#            "unit": component["valueQuantity"]["unit"],
+#            "value": component["valueQuantity"]["value"],
+#            "display": coding["display"]
+#          }
+#        else
+#          # note: not clear that we need this, because there could conceivably
+#          # be codes--like codes from other systems--that are present in
+#          # addition to the ones we want, and we probably don't need to error
+#          # on that.
+#          raise "not systolic or diastolic"
+#        end
+      # end
+    # end
+
+
+    if systolic.blank? || diastolic.blank?
+      # TODO: unlike the above error, I do think we need this one, because if
+      # either are missing from the entry I don't think we can use it.
+      # However, it's possible that there may be entire entries that we could
+      # skip if we still got some valid entries, so again I'm not certain that
+      # raising an error here is correct.
+      raise "missing systolic or diastolic"
+    else
+      result[:systolic] = extract_bp_data_from_component(systolic)
+      result[:diastolic] = extract_bp_data_from_component(diastolic)
+      # result[:diastolic] = diastolic
+    end
+
+    return result
+
+  end
+
+  def filter_components_by_code(code, components)
+    # Filter the components to only those that have at least one code.coding element with the code:
+    matches = components.filter {|item| item["code"]["coding"].filter {|el| el["code"] == code}.length >0}
+    # Filter the code.coding list to only have elements matching the code:
+    matches.map {|match| match["code"]["coding"] = match["code"]["coding"].filter {|el| el["code"] == code}}
+    return matches
+  end
+
+  def extract_bp_data_from_component(component)
+    # component.code.coding, since we've filtered it down in filter_components_by_code,
+    # should only the coding we expect, and since if there were multiples for some odd
+    # reason the values in them would all be the same, we can just take the first one.
+    coding = pick(["code", "display"], component["code"]["coding"].first)
+    # The values we want are all in component.valueQuantity
+    values = pick(["unit", "value"], component["valueQuantity"])
+    data = coding.merge(values)
+    return data
+  end
+
+end
+
