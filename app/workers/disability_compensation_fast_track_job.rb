c3d0ec4c8502f3a32e181142233745b7879c951e
Tadhg O'Higgins <tadhg@tadhg.com>
2021-11-19 17:09:30 -0800

    Rubocop corrections and spec split out for HypertensionObservationData class testing.

--- app/workers/disability_compensation_fast_track_job.rb
+++ app/workers/disability_compensation_fast_track_job.rb
@@ -11,15 +11,16 @@ class DisabilityCompensationFastTrackJob
   def perform(form526_submission_id)
     # submission = Form526Submission.find(form526_submission_id)
     # icn = Account.where(idme_uuid: submission.user_uuid).first.icn
-    #temporary below
+    # temporary below
     icn = 2000163
     client = Lighthouse::VeteransHealth::Client.new
     # TODO: rescue !=200 responses with an appropriate action
     condition_response = client.get_request('conditions', icn)
-    return unless is_hypertension?(condition_response)
+    return unless hypertension?(condition_response)
+
     # TODO: rescue !=200 responses with an appropriate action
-    results = HypertensionObservationData.new(observations_response).transform
     observations_response = client.get_request('observations', icn)
+    results = HypertensionObservationData.new(observations_response).transform
     # entries = observations_response.body.dig('entry')
     # results = entries.map {|entry| transform_entry(entry)}
 
@@ -28,9 +29,8 @@ class DisabilityCompensationFastTrackJob
     # client.upload(pdf_body, create_document_data(upload_data))
   end
 
-
-  def is_hypertension?(condition_response)
-    condition_response.body['entry'].each { |e| return true if e['resource']['code']['text'].downcase == 'hypertension' }
+  def hypertension?(condition_response)
+    condition_response.body['entry'].each { |e| true if e['resource']['code']['text'].downcase == 'hypertension' }
   end
 
   def generate_pdf(_condition_response)
@@ -56,7 +56,13 @@ end
 # What should the DisabilityCompensationFastTrackJob class do, as opposed to helper class(es).
 # 1. Get the conditions data about the claim.
 # 2. If conditions data does not match hypertension, do nothing.
-# 3. Otherwise: call LH for more data to get BP and medication data; parse that data; shape that data; generate a PDF from that data; attach PDF to EVSS; attach special issue (RRD) to EVSS claim; submit EVSS claim. (Not in that order necessarily)
+# 3. Otherwise: call LH for more data to get BP and medication data;
+#    parse that data;
+#    shape that data;
+#    generate a PDF from that data;
+#    attach PDF to EVSS;
+#    attach special issue (RRD) to EVSS claim;
+#    submit EVSS claim. (Not in that order necessarily)
 # Helper classes to:
 # - parse LH API call, shape, return it.
 
@@ -68,45 +74,39 @@ class HypertensionObservationData
   end
 
   def transform
-    entries = response.body.dig('entry')
-    entries.map {|entry| transform_entry(entry)}
+    entries = response.body['entry']
+    entries.map { |entry| transform_entry(entry) }
   end
 
   private
 
   def pick(keys, hash)
-    result = hash.select {|k, v| keys.include? k}
-    return result
+    hash.select { |k, _| keys.include? k }
   end
 
   def transform_entry(raw_entry)
     # TODO: DO we need to verify LOINC code 85354-9 here as well?
     # TODO: I'm using issued here over effectiveDateTime, is that correct?
-    entry = pick(["issued", "component", "performer"], raw_entry["resource"])
-    result = {"issued": entry["issued"]}
-    practitioner_hash = get_display_hash_from_performer("Practitioner", entry)
-    organization_hash = get_display_hash_from_performer("Organization", entry)
+    entry = pick(%w[issued component performer], raw_entry['resource'])
+    result = { 'issued': entry['issued'] }
+    practitioner_hash = get_display_hash_from_performer('Practitioner', entry)
+    organization_hash = get_display_hash_from_performer('Organization', entry)
     bp_hash = get_bp_readings_from_entry(entry)
-    result = result.merge(practitioner_hash, organization_hash, bp_hash)
-    return result
+    result.merge(practitioner_hash, organization_hash, bp_hash)
   end
 
   def get_display_hash_from_performer(term, entry)
     result = {}
-    performer = entry["performer"].select {|item| item["reference"].include? term}.first
-    if performer.present?
-      result[term.downcase.to_sym] = performer["display"]
-    end
-    return result
-
+    performer = entry['performer'].select { |item| item['reference'].include? term }.first
+    result[term.downcase.to_sym] = performer['display'] if performer.present?
+    result
   end
 
   def get_bp_readings_from_entry(entry)
-    
     result = {}
     # Each component should contain a BP pair, so after filtering there should only be one reading of each type:
-    systolic = filter_components_by_code("8480-6", entry["component"]).first
-    diastolic = filter_components_by_code("8462-4", entry["component"]).first
+    systolic = filter_components_by_code('8480-6', entry['component']).first
+    diastolic = filter_components_by_code('8462-4', entry['component']).first
 
     if systolic.blank? || diastolic.blank?
       # TODO: unlike the above error, I do think we need this one, because if
@@ -114,35 +114,31 @@ class HypertensionObservationData
       # However, it's possible that there may be entire entries that we could
       # skip if we still got some valid entries, so again I'm not certain that
       # raising an error here is correct.
-      raise "missing systolic or diastolic"
+      raise 'missing systolic or diastolic'
     else
       result[:systolic] = extract_bp_data_from_component(systolic)
       result[:diastolic] = extract_bp_data_from_component(diastolic)
       # result[:diastolic] = diastolic
     end
 
-    return result
-
+    result
   end
 
   def filter_components_by_code(code, components)
     # Filter the components to only those that have at least one code.coding element with the code:
-    matches = components.filter {|item| item["code"]["coding"].filter {|el| el["code"] == code}.length >0}
+    matches = components.filter { |item| item['code']['coding'].filter { |el| el['code'] == code }.length.positive? }
     # Filter the code.coding list to only have elements matching the code:
-    matches.map {|match| match["code"]["coding"] = match["code"]["coding"].filter {|el| el["code"] == code}}
-    return matches
+    matches.map { |match| match['code']['coding'] = match['code']['coding'].filter { |el| el['code'] == code } }
+    matches
   end
 
   def extract_bp_data_from_component(component)
     # component.code.coding, since we've filtered it down in filter_components_by_code,
     # should only the coding we expect, and since if there were multiples for some odd
     # reason the values in them would all be the same, we can just take the first one.
-    coding = pick(["code", "display"], component["code"]["coding"].first)
+    coding = pick(%w[code display], component['code']['coding'].first)
     # The values we want are all in component.valueQuantity
-    values = pick(["unit", "value"], component["valueQuantity"])
-    data = coding.merge(values)
-    return data
+    values = pick(%w[unit value], component['valueQuantity'])
+    coding.merge(values)
   end
-
 end
-
--- spec/jobs/disability_compensation_fast_track_job_spec.rb
+++ spec/jobs/disability_compensation_fast_track_job_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'ostruct'
 
 RSpec.describe DisabilityCompensationFastTrackJob, type: :job do
   subject { described_class }
@@ -11,7 +10,7 @@ RSpec.describe DisabilityCompensationFastTrackJob, type: :job do
   end
 
   let!(:user) { FactoryBot.create(:user, :loa3) }
-  #let!(:account) { FactoryBot.create(:account, icn: user.icn, idme_uuid: user.uuid) }
+  # let!(:account) { FactoryBot.create(:account, icn: user.icn, idme_uuid: user.uuid) }
   let(:auth_headers) do
     EVSS::DisabilityCompensationAuthHeaders.new(user).add_headers(EVSS::AuthHeaders.new(user).to_h)
   end
@@ -25,139 +24,54 @@ RSpec.describe DisabilityCompensationFastTrackJob, type: :job do
   end
 
   describe '#perform' do
-    #Once we have the logic for pinging lighthouse merged into our class, we should have our own cassette for the query in question 🤷<200d>♀️ OR do we KNOW that this 'lighthouse/clinical_health/condition_success' includes hypertension? 
+    # Once we have the logic for pinging lighthouse merged into our class, we
+    # should have our own cassette for the query in question 🤷<200d>♀️ OR do we
+    # KNOW that this 'lighthouse/clinical_health/condition_success' includes
+    # hypertension?
     #
-    #it 'soemthing' do
+    # it 'soemthing' do
     #  VCR.use_cassette('lighthouse/clinical_health/condition_success') do
     #    subject.perform_async(submission.id)
     #    described_class.drain
     #  end
-    #end
+    # end
 
     context 'success' do
       context 'the claim is NOT for hypertension' do
         it 'does nothing' do
           subject.perform_async(submission.id)
-          raise "not implemented"
-        end
-      end
-
-      context 'the claim IS for hypertension' do
-        it 'calls #new on Lighthouse::ClinicalHelth::Client' do
-        end
-
-        it 'parses the response from Lighthouse::ClinicalHelth::Client' do
-          ########
-        end
-
-        it 'generates a pdf' do
-          raise "not implemented"
-        end
-
-        it 'includes the neccesary information in the pdf' do
-          raise "not implemented"
-        end
-
-        it 'calls #upload on EVSS::DocumentsService with the expected argumnets' do
-          raise "not implemented"
+          raise 'not implemented'
         end
       end
     end
+
+    #      context 'the claim IS for hypertension' do
+    #        it 'calls #new on Lighthouse::ClinicalHealth::Client' do
+    #        end
+    #
+    #        it 'parses the response from Lighthouse::ClinicalHelth::Client' do
+    #          ########
+    #        end
+    #
+    #        it 'generates a pdf' do
+    #          raise 'not implemented'
+    #        end
+    #
+    #        it 'includes the neccesary information in the pdf' do
+    #          raise 'not implemented'
+    #        end
+    #
+    #        it 'calls #upload on EVSS::DocumentsService with the expected argumnets' do
+    #          raise 'not implemented'
+    #        end
+    #      end
 
     context 'failure' do
       it 'raises a helpful error' do
         allow(Lighthouse::VeteransHealth::Client).to receive(:new).and_return nil
         subject.perform_async(submission.id)
-        raise "not implemented"
+        raise 'not implemented'
       end
     end
-  end
-end
-
-RSpec.describe HypertensionObservationData do
-  subject { described_class }
-
-  let(:response) do
-    client = Lighthouse::VeteransHealth::Client.new
-    # Using specific test ICN below:
-    client.get_request('observations', 2000163)
-  end
-
-  before(:all) do
-    VCR.configure { |c| c.allow_http_connections_when_no_cassette = true }
-  end
-
-  after(:all) do
-    VCR.configure { |c| c.allow_http_connections_when_no_cassette = false }
-  end
-
-
-
-  describe "#transform" do
-    VCR.use_cassette('lighthouse/clinical_health/condition_success') do
-      it "returns the expected hash" do
-        expect(described_class.new(response).transform)
-          .to eq(
-[{ issued: "2009-03-23T01:15:52Z", practitioner: "DR. THOMAS359 REYNOLDS206 PHD", organization: "LYONS VA MEDICAL CENTER", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 115.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 87.0, "unit" => "mm[Hg]" } }, { issued: "2010-03-29T01:15:52Z", practitioner: "DR. JANE460 DOE922 MD", organization: "WASHINGTON VA MEDICAL CENTER", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 102.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 70.0, "unit" => "mm[Hg]" } }, { issued: "2011-04-04T01:15:52Z", organization: "NEW AMSTERDAM CBOC", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 137.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 86.0, "unit" => "mm[Hg]" } }, { issued: "2012-04-09T01:15:52Z", organization: "LYONS VA MEDICAL CENTER", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 124.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 80.0, "unit" => "mm[Hg]" } }, { issued: "2013-04-15T01:15:52Z", practitioner: "DR. JOHN248 SMITH811 MD", organization: "NEW AMSTERDAM CBOC", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 156.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 118.0, "unit" => "mm[Hg]" } }, { issued: "2014-04-21T01:15:52Z", practitioner: "DR. JANE460 DOE922 MD", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 192.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 93.0, "unit" => "mm[Hg]" } }, { issued: "2017-04-24T01:15:52Z", practitioner: "DR. JANE460 DOE922 MD", organization: "WASHINGTON VA MEDICAL CENTER", systolic: { "code" => "8480-6", "display" => "Systolic blood pressure", "value" => 153.0, "unit" => "mm[Hg]" }, diastolic: { "code" => "8462-4", "display" => "Diastolic blood pressure", "value" => 99.0, "unit" => "mm[Hg]" } }]
-          )
-      end
-    end
-      it "returns the expected hash" do
-        res = OpenStruct.new
-        res.body = {"entry" => []}
-        expect(described_class.new(res).transform)
-          .to eq( [] )
-      end
-      it "returns the expected hash" do
-        res = OpenStruct.new
-        res.body = {"entry" => [{"resource" => {
-          "issued" => "whatever",
-      "performer"=>
-       [{"reference"=>
-          "https://sandbox-api.va.gov/services/fhir/v0/r4/Organization/I2-AKOTGEFSVKFJOPUKHIVJAH5VQU000000",
-         "display"=>"NEW AMSTERDAM CBOC"}],
-      "component"=>
-       [{"code"=>
-          {"coding"=>
-            [{"system"=>"http://loinc.org",
-              "code"=>"8480-6",
-              "display"=>"Systolic blood pressure"}],
-           "text"=>"Systolic blood pressure"},
-         "valueQuantity"=>
-          {"value"=>153.0,
-           "unit"=>"mm[Hg]",
-           "system"=>"http://unitsofmeasure.org",
-           "code"=>"mm[Hg]"}},
-        {"code"=>
-          {"coding"=>
-            [{"system"=>"http://loinc.org",
-              "code"=>"8462-4",
-              "display"=>"Diastolic blood pressure"}],
-           "text"=>"Diastolic blood pressure"},
-         "valueQuantity"=>
-          {"value"=>99.0,
-           "unit"=>"mm[Hg]",
-           "system"=>"http://unitsofmeasure.org",
-           "code"=>"mm[Hg]"}}],
- 
-        }}]}
-        expect(described_class.new(res).transform)
-          .to eq( [{
-            "issued": "whatever",
-            "organization": "NEW AMSTERDAM CBOC",
-              "systolic": {
-                "code" => "8480-6",
-                "display" => "Systolic blood pressure",
-                "value" => 153.0,
-                "unit" => "mm[Hg]"
-              },
-              "diastolic": {
-                "code" => "8462-4",
-                "display" => "Diastolic blood pressure",
-                "value" => 99.0,
-                "unit" => "mm[Hg]"
-              },
-          }] )
-      end
   end
 end
