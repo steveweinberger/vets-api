a4bbddcc527ada14f22617bc0f688b7ac3b9e734
Julia Allen <julia.l.allen@gsa.gov>
2021-11-22 18:30:02 -0800

    refactor and add specs

--- lib/lighthouse/veterans_health/configuration.rb
+++ lib/lighthouse/veterans_health/configuration.rb
@@ -6,7 +6,6 @@ require 'common/client/middleware/response/raise_error'
 module Lighthouse
   module VeteransHealth
     class Configuration < Common::Client::Configuration::REST
-
       def base_path
         Settings.lighthouse.veterans_health.url
       end
@@ -15,12 +14,9 @@ module Lighthouse
         'Lighthouse_VeteransHealth'
       end
 
-      # def self.base_request_headers
-      #   super.merge('apiKey' => Settings.lighthouse.facilities.api_key)
-      # end
-
+      # TODO: check to see if this is compatible with va.gov's secrets management/retrieval strategy
       def rsa_key
-        @key ||= OpenSSL::PKey::RSA.new(File.read(Dir.home + Settings.lighthouse.veterans_health.api_key))
+        @key ||= OpenSSL::PKey::RSA.new(File.read(Dir.home + Settings.lighthouse.veterans_health.fast_tracker.api_key))
       end
 
       def connection
@@ -29,8 +25,8 @@ module Lighthouse
           faraday.use Faraday::Response::RaiseError
 
           # Uncomment this if you want curl command equivalent or response output to log
-          faraday.request(:curl, ::Logger.new(STDOUT), :warn) unless Rails.env.production?
-          faraday.response(:logger, ::Logger.new(STDOUT), bodies: true) unless Rails.env.production?
+          # faraday.request(:curl, ::Logger.new(STDOUT), :warn) unless Rails.env.production?
+          # faraday.response(:logger, ::Logger.new(STDOUT), bodies: true) unless Rails.env.production?
 
           faraday.response :json
           faraday.adapter Faraday.default_adapter
