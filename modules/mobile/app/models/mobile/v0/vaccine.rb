021fafd3d2fa7aab3c6e00043ca856beb90531ab
Kris Pethtel <kris.pethtel@gmail.com>
2021-11-24 11:57:58 -0500

    remove unnecessary code

--- modules/mobile/app/models/mobile/v0/vaccine.rb
+++ modules/mobile/app/models/mobile/v0/vaccine.rb
@@ -9,19 +9,6 @@ module Mobile
     class Vaccine < ApplicationRecord
       validates :cvx_code, presence: true, uniqueness: true
       validates :group_name, presence: true
-
-      # Adds incoming name to group name unless it's already included
-      # does not save record
-      # @param name [String] the group name that is being added
-      # @return [String] the group name
-      def add_group_name(name)
-        if group_name
-          self.group_name += ", #{name}" unless group_name.split(',').map(&:strip).include?(name)
-        else
-          self.group_name = name
-        end
-        group_name
-      end
     end
   end
 end
