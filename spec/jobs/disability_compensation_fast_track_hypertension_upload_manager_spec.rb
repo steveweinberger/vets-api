01a7d3576d5971cf476e53fd4200cb74ebaf9d00
Tasha Ibrahim <natasha.joyce.ibrahim@gmail.com>
2021-11-30 15:42:53 -0500

    Wrap up specs

--- spec/jobs/disability_compensation_fast_track_hypertension_upload_manager_spec.rb
+++ spec/jobs/disability_compensation_fast_track_hypertension_upload_manager_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe HypertensionUploadManager do
   let(:original_form_json_uploads) do
     JSON.parse(original_form_json)['form526_uploads']
   end                                                                                     
-  
+
   let(:form526_submission) do 
     Form526Submission.create(
       user_uuid: user.uuid,
@@ -30,18 +30,22 @@ RSpec.describe HypertensionUploadManager do
     context 'success' do
       it 'appends the new upload and saves the expected JSON' do
         HypertensionUploadManager.new(form526_submission).add_upload("fake_confirmation_code")
-        expect(form526_submission.form_json).not_to eq original_form_json
         parsed_json = JSON.parse(form526_submission.form_json)['form526_uploads']
+
         expect(parsed_json).to match original_form_json_uploads + [{ 'name'=> 'hypertension_evidence.pdf', 'confirmationCode'=> 'fake_confirmation_code', 'attachmentId'=> '1489' }]
       end
 
-      it 'adds upload files if no existing list is present' do
-       # original = JSON.parse(form526_submission.form_json)
-       # testdata = original.select { |k, _| k != 'form526_upload' }
-       # form526_submission.form_json = JSON.dump(testdata)
-       # uploads = JSON.parse(HypertensionUploadManager.new(form526_submission, '123').add_upload)['form526_uploads']
-       # expected_files = [{ 'name'=> 'hypertension_evidence.pdf', 'confirmationCode'=> '123', 'attachmentId'=> '1489' }]
-       # expect(uploads).to match expected_files
+      context 'there are no existing uploads present in the list' do
+        let(:original_form_json) do
+          File.read('spec/support/disability_compensation_form/submissions/without_form526_uploads.json')
+        end
+
+        it 'adds the new upload' do
+          HypertensionUploadManager.new(form526_submission).add_upload("fake_confirmation_code")
+          parsed_json = JSON.parse(form526_submission.form_json)['form526_uploads']
+
+          expect(parsed_json).to match [{ 'name'=> 'hypertension_evidence.pdf', 'confirmationCode'=> 'fake_confirmation_code', 'attachmentId'=> '1489' }]
+        end
       end
     end
   end
