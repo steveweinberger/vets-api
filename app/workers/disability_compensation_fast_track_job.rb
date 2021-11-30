8851c0ee9fcc363d94be21fe8290bb7f34404852
Tadhg O'Higgins <tadhg@tadhg.com>
2021-11-30 09:23:19 -0800

    Update PDF styling.

--- app/workers/disability_compensation_fast_track_job.rb
+++ app/workers/disability_compensation_fast_track_job.rb
@@ -8,8 +8,9 @@ class DisabilityCompensationFastTrackJob
   include Sidekiq::Worker
 
   extend SentryLogging
-  # TODO: This needs to be thought about more. How many retries and how long do we want this to attempt.
-  sidekiq_options retry: 14
+  # TODO: This is apparently at most about 4.5 hours.
+  # https://github.com/mperham/sidekiq/issues/2168#issuecomment-72079636
+  sidekiq_options retry: 8
 
   sidekiq_retries_exhausted do |msg, _ex|
     submission_id = msg['args'].first
@@ -254,34 +255,42 @@ class HypertensionPDFGenerator
     patient_name = stringify_patient
     #gen_stamp = '09/01/2021 at 10:23am EST' # TODO: fix when I figure out how to do Ruby time manipulation
     gen_time = Time.now
-    gen_stamp = "#{gen_time.strftime("%m/%d/%Y")} at #{gen_time.strftime("%I:%M %p")}"
+    gen_stamp = "#{gen_time.strftime("%m/%d/%Y")} at #{gen_time.strftime("%l:%M %p %Z")}"
 
     intro_lines = [
-      "<b><font size='8'>Hypertension Rapid Ready for Decision | Claim for Increase</font></b>\n",
-      "<font size='16'>VHA Hypertension Data Summary for</font>",
-      "<font size='16'>#{patient_name}</font>\n",
-      "<font size='8'><i>#{gen_stamp}<i>\n"
+      "<font size='11'>Hypertension Rapid Ready for Decision | Claim for Increase</font>\n",
+      "<font size='22'>VHA Hypertension Data Summary for</font>",
+      "<font size='22'>#{patient_name}</font>\n",
+      "<font size='10'><i>Generated automatically on #{gen_stamp}<i>\n"
     ]
 
     intro_lines.each do |line|
       pdf.text line, inline_format: true
     end
 
-    pdf.text "\n", size: 12
+    pdf.text "\n", size: 10
 
     pdf
   end
 
   def add_blood_pressure(pdf)
+    with_intro = add_blood_pressure_intro(pdf)
+    with_bp = add_blood_pressure_list(with_intro)
+    with_outro = add_blood_pressure_outro(with_bp)
+    with_outro
+  end
+
+  def add_blood_pressure_intro(pdf)
     header = bp_data.length.positive? ? 'One Year of Blood Pressure History' : 'No blood pressure records found'
-    bp_note = bp_data.length.positive? ? "<font size='8'>Blood pressure is shown as systolic/diastolic.\n</font>" : ''
+    bp_note = bp_data.length.positive? ? "<font size='11'>Blood pressure is shown as systolic/diastolic.\n</font>" : ''
     end_date= @date.strftime("%m/%d/%Y")
     start_date = (@date - 1.year).strftime("%m/%d/%Y")
     search_window = "VHA records searched from #{start_date} to #{end_date}"
     bp_intro_lines = [
-      "<font size='14'>#{header}</font>",
-      "<font size='8'><i>#{search_window}<i></font>",
-      "<font size='8'><i>All VAMC locations using VistA/CAPRI were checked.<i></font>",
+      "<font size='16'>#{header}</font>",
+      "<font size='11'><i>#{search_window}<i></font>",
+      "<font size='11'><i>All VAMC locations using VistA/CAPRI were checked<i></font>",
+      "\n",
       bp_note
     ]
 
@@ -293,7 +302,27 @@ class HypertensionPDFGenerator
       return pdf
     end
 
+    pdf.text "\n", size: 10
+
+    pdf
+  end
+
+  def add_blood_pressure_list(pdf)
+    @bp_data.each do |bp|
+      pdf.text "<b>Blood pressure: #{bp[:systolic]['value']}/#{bp[:diastolic]['value']} #{bp[:systolic]['unit']}", inline_format: true, size: 11
+      pdf.text "Taken on: #{bp[:issued][0, 10].to_date.strftime('%m/%d/%Y')}", size: 11
+      pdf.text "Location: #{bp[:organization] || 'Unknown'}", size: 11
+      pdf.text "\n", size: 8
+    end
+
     pdf.text "\n", size: 12
+
+    pdf
+  end
+
+  def add_blood_pressure_table(pdf)
+    # The table version of the medications, which we may need for future user
+    # testing.
     bp_rows = [['<b>Blood pressure</b>', '<b>Date</b>', '<b>Location</b>']]
     @bp_data.each do |bp|
       bp_rows.append([
@@ -304,39 +333,49 @@ class HypertensionPDFGenerator
     end
     pdf.table(bp_rows, cell_style: { size: 8, inline_format: true })
 
+    pdf
+  end
+
+  def add_blood_pressure_outro(pdf)
     pdf.text "\n", size: 12
 
-    pdf.text 'Hypertension Rating Schedule', size: 12
+    pdf.text 'Hypertension Rating Schedule', size: 14
 
     pdf.table([
                 [
                   '10%',
-                  'Systolic pressure predominantly 160 or more; or diastolic pressure predominantly 100 or more; or minimum evaluation for an individual with a history of diastolic pressure predominantly 100 or more who requires continuous medication for control.'
+                  'Systolic pressure predominantly 160 or more; or diastolic pressure predominantly 100 or more; or minimum evaluation for an individual with a history of diastolic pressure predominantly 100 or more who requires continuous medication for control'
                 ],
                 [
-                  '20%', 'Systolic pressure predominantly 200 or more; or diastolic pressure predominantly 110 or more.'
+                  '20%', 'Systolic pressure predominantly 200 or more; or diastolic pressure predominantly 110 or more'
                 ],
                 [
-                  '40%', 'Diastolic pressure 120 or more.'
+                  '40%', 'Diastolic pressure 120 or more'
                 ],
                 [
-                  '60%', 'Diastolic pressure 130 or more.'
+                  '60%', 'Diastolic pressure 130 or more'
                 ]
-              ], cell_style: { size: 8 })
+              ], width: 350, column_widths: [30, 320], cell_style: { size: 10, border_width: 0, background_color: "f3f3f3" })
 
     pdf.text "\n"
     pdf.text "<link href='https://www.ecfr.gov/current/title-38/chapter-I/part-4'>View rating schedule</link>",
-             inline_format: true, color: '0000ff', size: 7
+             inline_format: true, color: '0000ff', size: 11
 
     pdf
 
   end
 
   def add_medications(pdf)
-    pdf.text "\n", size: 12
-    pdf.text 'Active Prescriptions', size: 14
+    pdf = add_medications_intro(pdf)
+    pdf = add_medications_list(pdf)
+    pdf
+  end
 
-    med_search_window = "VHA records searched for medication prescriptions active as of #{Date.today.strftime('%m/%d/%Y')}."
+  def add_medications_intro(pdf)
+    pdf.text "\n", size: 11
+    pdf.text 'Active Prescriptions', size: 16
+
+    med_search_window = "VHA records searched for medication prescriptions active as of #{Date.today.strftime('%m/%d/%Y')}"
     prescription_lines = [
       med_search_window,
       'All VAMC locations using VistA/CAPRI were checked',
@@ -344,9 +383,26 @@ class HypertensionPDFGenerator
     ]
 
     prescription_lines.each do |line|
-      pdf.text line, size: 8, style: :italic
+      pdf.text line, size: 11, style: :italic
     end
 
+    pdf
+  end
+
+  def add_medications_list(pdf)
+    @medications.each do |medication|
+      pdf.text medication['description'], size: 11, style: :bold
+      pdf.text "Prescribed on: #{medication['authoredOn'][0, 10].to_date.strftime('%m/%d/%Y')}"
+      pdf.text "Dosages instructions: #{medication['dosageInstructions'].join('; ')}"
+      pdf.text "\n", size: 8
+    end
+
+    pdf
+  end
+
+  def add_medications_table(pdf)
+    # The table version of the medications, which we may need for future user
+    # testing.
     med_rows = [[
       '<b>Medication</b>',
       '<b>Prescribed on</b>',
@@ -365,18 +421,19 @@ class HypertensionPDFGenerator
   end
 
   def add_about(pdf)
+    pdf.text  "\n"
+    pdf.text  'About this Document', size: 14
     about_lines = [
+      'The Hypertension Rapid Ready for Decision system retrieves and summarizes VHA medical records related to hypertension claims for increase submitted on va.gov. VSRs and RVSRs can develop and rate this claim without ordering an exam if there is sufficient existing evidence to show predominance according to <link href="https://www.ecfr.gov/current/title-38/part-4"><color rgb="0000ff">DC 7101 (Hypertension) Rating Criteria</color></link>. This is not new guidance, but rather a way to <link href="https://www.ecfr.gov/current/title-38/chapter-I/part-3/subpart-A/subject-group-ECFR7629a1b1e9bf6f8/section-3.159"><color rgb="0000ff">operationalize existing statutory rules</color><link> in 38 U.S.C § 5103a(d).',
       "\n",
-      'About this document',
-      'The Hypertension Rapid Ready for Decision system retrieves and summarizes VHA medical records related to hypertension claims for increase submitted on va.gov. VSRs and RVSRs can develop and rate this claim without ordering an exam if there is sufficient existing evidence to show predominance according to DC 7101 (Hypertension) Rating Criteria. This is not new guidance, but rather a way to operationalize existing statutory rules in 38 U.S.C § 5103a(d).',
       'Not included in this document:',
-      ' -  Private medical records',
-      ' -  VAMC data for clinics using CERNER Electronic Health Record system (Replacing VistA, but currently only used at Mann-Grandstaff VA Medical Center in Spokane, Washington)',
-      ' -  JLV/Department of Defense medical records'
+      ' •  Private medical records',
+      ' •  VAMC data for clinics using CERNER Electronic Health Record system (Replacing VistA, but currently only used at Mann-Grandstaff VA Medical Center in Spokane, Washington)',
+      ' •  JLV/Department of Defense medical records'
     ]
 
     about_lines.each do |line|
-      pdf.text line, size: 6
+      pdf.text line, size: 11, inline_format: true
     end
 
     pdf
