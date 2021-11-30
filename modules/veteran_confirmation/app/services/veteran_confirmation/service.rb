88c99ce715fda85c677fbc98f2665401f3123b66
Josh Scanish <josh.scanish@oddball.io>
2021-11-30 14:52:15 -0500

    Fix failed tests by avoiding problematic validation

--- modules/veteran_confirmation/app/services/veteran_confirmation/service.rb
+++ modules/veteran_confirmation/app/services/veteran_confirmation/service.rb
@@ -6,5 +6,11 @@ require 'mpi/configuration'
 module VeteranConfirmation
   class Service < MPI::Service
     configuration MPI::Configuration
+
+    # This method is a simplified version of the one found in MPI::Service. It exists largely to avoid a
+    # problematic validation will cause the MPI call to fail.
+    def create_profile_message(user_identity, search_type: MPI::Constants::CORRELATION_WITH_RELATIONSHIP_DATA)
+      message_user_attributes(user_identity, search_type)
+    end
   end
 end
