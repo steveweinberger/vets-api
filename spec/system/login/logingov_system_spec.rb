dfbad9f938122def18ee6d982696975de25f4def
Josh Scanish <josh.scanish@oddball.io>
2021-12-01 16:08:45 -0500

    Add tests

--- spec/system/login/logingov_system_spec.rb
+++ spec/system/login/logingov_system_spec.rb
@@ -6,15 +6,19 @@ require 'rotp'
 if ENV['LOGIN_SYSTEM_TESTS']
   RSpec.describe 'Login.gov login', type: :system do
     context 'outbound' do
-      include_examples('logs in outbound Login.gov user',
-                       'joe.niquette+lgovial1a@oddball.io',
-                       ENV['LOGINGOV_IAL1_PASSWORD'],
-                       ENV['MFA_IAL1_KEY'])
+      context 'IAL1' do
+        include_examples('logs in outbound Login.gov user',
+                         'joe.niquette+lgovial1a@oddball.io',
+                         ENV['LOGINGOV_IAL1_PASSWORD'],
+                         ENV['MFA_IAL1_KEY'])
+      end
 
-      include_examples('logs in outbound Login.gov user',
-                       'joe.niquette+lgovial2a@oddball.io',
-                       ENV['LOGINGOV_IAL2_PASSWORD'],
-                       ENV['MFA_IAL2_KEY'])
+      context 'IAL2' do
+        include_examples('logs in outbound Login.gov user',
+                         'joe.niquette+lgovial2a@oddball.io',
+                         ENV['LOGINGOV_IAL2_PASSWORD'],
+                         ENV['MFA_IAL2_KEY'])
+      end
     end
   end
 end
