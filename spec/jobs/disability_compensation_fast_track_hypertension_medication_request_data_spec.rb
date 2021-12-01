d7f146d382fb8179a3f31e93bef96c8da8d94935
Tadhg O'Higgins <tadhg@tadhg.com>
2021-12-01 12:42:22 -0800

    Make medication request spec look better.

--- spec/jobs/disability_compensation_fast_track_hypertension_medication_request_data_spec.rb
+++ spec/jobs/disability_compensation_fast_track_hypertension_medication_request_data_spec.rb
@@ -22,59 +22,59 @@ RSpec.describe HypertensionMedicationRequestData, :vcr do
     end
 
     it 'returns the expected hash from a single-entry list' do
-      expect(described_class.new(response).transform).to match([{ 'status' => 'active',
-                                                                  'authoredOn' => '1995-02-06T02:15:52Z',
-                                                                  'description' => 'Hydrochlorothiazide 6.25 MG',
-                                                                  'notes' => ['Hydrochlorothiazide 6.25 MG'],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '1995-04-30T01:15:52Z',
-                                                                  'description' => 'Loratadine 5 MG Chewable Tablet',
-                                                                  'notes' => ['Loratadine 5 MG Chewable Tablet'],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '1995-04-30T01:15:52Z',
-                                                                  'description' => '0.3 ML EPINEPHrine 0.5 '\
-                                                                                   'MG/ML Auto-Injector',
-                                                                  'notes' => [
-                                                                    '0.3 ML EPINEPHrine 0.5 MG/ML Auto-Injector'
-                                                                  ],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '1998-02-12T02:15:52Z',
-                                                                  'description' => '120 ACTUAT Fluticasone ' \
-                                                                                   'propionate 0.044 MG/ACTUAT ' \
-                                                                                   'Metered Dose Inhaler',
-                                                                  'notes' => [
-                                                                    '120 ACTUAT Fluticasone propionate 0.044 ' \
-                                                                    'MG/ACTUAT Metered Dose Inhaler'
-                                                                  ],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '1998-02-12T02:15:52Z',
-                                                                  'description' => '200 ACTUAT Albuterol 0.09 ' \
-                                                                                   'MG/ACTUAT Metered Dose Inhaler',
-                                                                  'notes' => ['200 ACTUAT Albuterol 0.09 MG/ACTUAT ' \
-                                                                              'Metered Dose Inhaler'],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '2009-03-25T01:15:52Z',
-                                                                  'description' => 'Hydrocortisone 10 MG/ML ' \
-                                                                                   'Topical Cream',
-                                                                  'notes' => ['Hydrocortisone 10 MG/ML Topical Cream'],
-                                                                  'dosageInstructions' => ['Once per day.'] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '2012-08-18T06:15:52Z',
-                                                                  'description' => 'predniSONE 5 MG Oral Tablet',
-                                                                  'notes' => ['predniSONE 5 MG Oral Tablet'],
-                                                                  'dosageInstructions' => [
-                                                                    '1 dose(s) 1 time(s) per 1 days'
-                                                                  ] },
-                                                                { 'status' => 'active',
-                                                                  'authoredOn' => '2013-04-15T01:15:52Z',
-                                                                  'description' => 'Hydrochlorothiazide 25 MG',
-                                                                  'notes' => ['Hydrochlorothiazide 25 MG'],
-                                                                  'dosageInstructions' => ['Once per day.'] }])
+      expect(described_class.new(response).transform).to match(
+        [
+          {
+            'status' => 'active',
+            'authoredOn' => '1995-02-06T02:15:52Z',
+            'description' => 'Hydrochlorothiazide 6.25 MG',
+            'notes' => ['Hydrochlorothiazide 6.25 MG'],
+            'dosageInstructions' => ['Once per day.']
+          },
+          { 'status' => 'active',
+            'authoredOn' => '1995-04-30T01:15:52Z',
+            'description' => 'Loratadine 5 MG Chewable Tablet',
+            'notes' => ['Loratadine 5 MG Chewable Tablet'],
+            'dosageInstructions' => ['Once per day.'] },
+          { 'status' => 'active',
+            'authoredOn' => '1995-04-30T01:15:52Z',
+            'description' => '0.3 ML EPINEPHrine 0.5 MG/ML Auto-Injector',
+            'notes' => [
+              '0.3 ML EPINEPHrine 0.5 MG/ML Auto-Injector'
+            ],
+            'dosageInstructions' => ['Once per day.'] },
+          { 'status' => 'active',
+            'authoredOn' => '1998-02-12T02:15:52Z',
+            'description' => '120 ACTUAT Fluticasone propionate 0.044 MG/ACTUAT Metered Dose Inhaler',
+            'notes' => [
+              '120 ACTUAT Fluticasone propionate 0.044 MG/ACTUAT Metered Dose Inhaler'
+            ],
+            'dosageInstructions' => ['Once per day.'] },
+          { 'status' => 'active',
+            'authoredOn' => '1998-02-12T02:15:52Z',
+            'description' => '200 ACTUAT Albuterol 0.09 MG/ACTUAT Metered Dose Inhaler',
+            'notes' => ['200 ACTUAT Albuterol 0.09 MG/ACTUAT Metered Dose Inhaler'],
+            'dosageInstructions' => ['Once per day.'] },
+          { 'status' => 'active',
+            'authoredOn' => '2009-03-25T01:15:52Z',
+            'description' => 'Hydrocortisone 10 MG/ML ' \
+                             'Topical Cream',
+            'notes' => ['Hydrocortisone 10 MG/ML Topical Cream'],
+            'dosageInstructions' => ['Once per day.'] },
+          { 'status' => 'active',
+            'authoredOn' => '2012-08-18T06:15:52Z',
+            'description' => 'predniSONE 5 MG Oral Tablet',
+            'notes' => ['predniSONE 5 MG Oral Tablet'],
+            'dosageInstructions' => [
+              '1 dose(s) 1 time(s) per 1 days'
+            ] },
+          { 'status' => 'active',
+            'authoredOn' => '2013-04-15T01:15:52Z',
+            'description' => 'Hydrochlorothiazide 25 MG',
+            'notes' => ['Hydrochlorothiazide 25 MG'],
+            'dosageInstructions' => ['Once per day.'] }
+        ]
+      )
     end
   end
 end
