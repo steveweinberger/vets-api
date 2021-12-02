be40c8428b8e5c2ed0cceb9ddedfa817749d07a6
Josh Scanish <josh.scanish@oddball.io>
2021-12-02 15:37:40 -0500

    Remove unnecessary file requirements

--- modules/veteran_confirmation/app/services/veteran_confirmation/service.rb
+++ modules/veteran_confirmation/app/services/veteran_confirmation/service.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-require 'mpi/service'
-require 'mpi/configuration'
-
 module VeteranConfirmation
   class Service < MPI::Service
     configuration MPI::Configuration
 
     # This method is a simplified version of the one found in MPI::Service. It exists largely to avoid a
-    # problematic validation will cause the MPI call to fail.
+    # problematic validation that will cause the MPI call to fail.
     def create_profile_message(user_identity, search_type: MPI::Constants::CORRELATION_WITH_RELATIONSHIP_DATA)
       message_user_attributes(user_identity, search_type)
     end
