cd69cccd37d5cfa9c9426f5dc6516501af12d1a5
Tasha Ibrahim <natasha.joyce.ibrahim@gmail.com>
2021-12-03 15:04:53 -0500

    Respond to feedback for the hypertension_observation_data class

--- app/workers/fast_track/hypertension_observation_data.rb
+++ app/workers/fast_track/hypertension_observation_data.rb
@@ -15,24 +15,20 @@ module FastTrack
 
     private
 
-    def pick(keys, hash)
-      hash.select { |k, _| keys.include? k }.with_indifferent_access
-    end
-
     def transform_entry(raw_entry)
-      entry = pick(%w[issued component performer], raw_entry['resource'])
-      result = { issued: entry['issued'] }
+      entry = raw_entry['resource'].slice("issued", "component", "performer")
       practitioner_hash = get_display_hash_from_performer('Practitioner', entry)
       organization_hash = get_display_hash_from_performer('Organization', entry)
       bp_hash = get_bp_readings_from_entry(entry)
-      result.merge(practitioner_hash, organization_hash, bp_hash)
+
+      { issued: entry['issued'] }.merge(practitioner_hash, organization_hash, bp_hash)
     end
 
     def get_display_hash_from_performer(term, entry)
       result = {}
       if entry['performer'].present?
-        performer_with_term = entry['performer'].select { |item| item['reference'].include? term }
-        result[term.downcase.to_sym] = performer_with_term.first['display'] if performer_with_term.present?
+        performer_with_term = entry['performer'].detect { |item| item['reference'].include? term }
+        result[term.downcase.to_sym] = performer_with_term['display'] if performer_with_term.present?
       end
       result
     end
@@ -69,9 +65,9 @@ module FastTrack
       # component.code.coding, since we've filtered it down in filter_components_by_code,
       # should only the coding we expect, and since if there were multiples for some odd
       # reason the values in them would all be the same, we can just take the first one.
-      coding = pick(%w[code display], component['code']['coding'].first)
+      coding = component['code']['coding'].first.slice("code", "display")
       # The values we want are all in component.valueQuantity
-      values = pick(%w[unit value], component['valueQuantity'])
+      values = component['valueQuantity'].slice("unit", "value")
       coding.merge(values)
     end
   end
