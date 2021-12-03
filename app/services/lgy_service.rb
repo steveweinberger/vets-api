14e40c7403fd199c151626eef37dd4d0604b483d
Kevin Musiorski <kevin.musiorski@gcio.com>
2021-12-02 19:48:49 -0600

    pass in edipi and icn keyword args to .new

--- app/services/lgy_service.rb
+++ app/services/lgy_service.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class LgyService
-
+  def initialize(edipi:, icn:)
+  end
 end
--- spec/services/lgy_service_spec.rb
+++ spec/services/lgy_service_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 describe LgyService do
   describe '#new' do
+    let(:user) { create :user }
+    subject { described_class.new(edipi: user.edipi, icn: user.icn) }
     it 'creates a new object of type LgyService' do
       expect(subject).to be_instance_of(LgyService)
     end
