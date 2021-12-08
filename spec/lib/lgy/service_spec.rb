717a21bc25a543a9cdea9043a5e12c4f445ddec9
Kevin Musiorski <kevin.musiorski@gcio.com>
2021-12-08 09:49:19 -0600

    LGY::Service   #get_determination     when response is eligible       response code is a 200       response body['determination_date'] exists       response body['status'] is ELIGIBLE

--- spec/lib/lgy/service_spec.rb
+++ spec/lib/lgy/service_spec.rb
@@ -4,34 +4,29 @@ require 'rails_helper'
 require 'lgy/service'
 
 describe LGY::Service do
+  let(:user) { FactoryBot.create(:evss_user, :loa3) }
 
+  describe '#get_determination' do
+    subject { described_class.new(edipi: user.edipi, icn: user.icn).get_determination }
 
+    context 'when response is eligible' do
+      before { VCR.insert_cassette 'lgy/determination_eligible' }
 
+      after { VCR.eject_cassette 'lgy/determination_eligible' }
 
-  # let(:user) { FactoryBot.create(:evss_user, :loa3) }
-  # let(:service) { LGY::Service.new(edipi: user.edipi, icn: user.icn) }
+      it 'response code is a 200' do
+        expect(subject.status).to eq 200
+      end
 
-  # describe '#get_coe_status' do
-  #   let(:faraday_response) { double('faraday_connection') }
+      it "response body['status'] is ELIGIBLE" do
+        expect(subject.body['status']).to eq 'ELIGIBLE'
+      end
 
-  #   before do
-  #     allow(faraday_response).to receive(:env)
-  #   end
-
-  #   context 'with an eligible determination' do
-  #     it 'successfully receives an eligible determination' do
-  #       VCR.use_cassette('lgy/determination_eligible') do
-  #         response = service.get_determination
-
-  #         expect(response.status).to eq(200)
-  #         expect(response.body['status']).to eq('ELIGIBLE')
-  #         expect(response.body['reference_number']).to eq('16934344')
-  #         # rubocop:disable Style/NumericLiterals
-  #         expect(response.body['determination_date']).to eq(1638569892000)
-  #         # rubocop:enable Style/NumericLiterals
-  #       end
-  #     end
-  #   end
+      it "response body['determination_date'] exists" do
+        expect(subject.body).to include 'determination_date'
+      end
+    end
+  end
 
   #   context 'with an automatically approved coe' do
   #     it 'does not find an application' do
